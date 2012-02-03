98881ed11f067e6c9239031d41ecf205c3d64053
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-02-03 14:43:51 +0900

    MOAIEnvironment snip

---
+++ moai/env.lua
@@ -0,0 +1,7 @@
+print( "devname:", MOAIEnvironment.getDevName() )
+print( "devmodel:", MOAIEnvironment.getDevModel() )
+print( "devproduct:", MOAIEnvironment.getDevProduct() )
+print( "osver:", MOAIEnvironment.getOSVersion() )
+
+print( "moaiapp:", MOAIApp )
+
