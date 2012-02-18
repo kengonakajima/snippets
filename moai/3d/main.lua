0cbe3a053d1aac9b9c0d0592478ad004bd38c432
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-02-18 17:18:52 +0900

    add 3d transparent img

---
+++ moai/3d/main.lua
@@ -0,0 +1,42 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAISim.openWindow ( "test", 320, 480 )
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( 320, 480 )
+viewport:setScale ( 320, 480 )
+
+layer = MOAILayer.new ()
+layer:setViewport ( viewport )
+MOAISim.pushRenderPass ( layer )
+
+gfxQuad = MOAIGfxQuad2D.new ()
+gfxQuad:setTexture ( "../sol.png" ) --"cathead.png" )
+gfxQuad:setRect ( -64, -64, 64, 64 )
+gfxQuad:setUVRect ( 0, 1, 1, 0 )
+
+prop = MOAIProp.new ()
+prop:setDeck ( gfxQuad )
+layer:insertProp ( prop )
+
+prop:moveRot ( 360, 180, 0, 3 )
+prop:moveLoc ( 0, 0, 128, 3 )
+
+
+prop2 = MOAIProp.new ()
+prop2:setDeck ( gfxQuad )
+layer:insertProp ( prop2 )
+
+prop2:moveRot ( 360, 180, 0, 8 )
+prop2:moveLoc ( 40, 0, 128, 3 )
+
+
+
+camera = MOAICamera3D.new ()
+camera:setLoc ( 0, 0, camera:getFocalLength ( 320 ))
+layer:setCamera ( camera )
+
