e30c95af5f9f54a4f88d908cf38ca10618f01909
kengo nakajima <kengo.nakajima@gmail.com>
2012-04-01 19:37:54 -0400

    smtp sample

--- ruby/mail/smtp.rb
+++ ruby/mail/smtp.rb
@@ -1,8 +1,46 @@
+
 require "net/smtp"
+require "kconv"
 
-sm = Net::SMTPSession.new( "localhost" , 25 )
-sm.start( "titan.co.jp" )
-sm.sendmail( "Subject: asdfasdfasdf\r\n\r\nhoge\r\n" , "ahoge" , "ringo@titan.co.jp" )
+def send(from,to,msg)
+  date = `date`.chop
 
-sm.finish
+  text  = "Subject: #{msg}\n"
+  text += "From: #{from}\n"
+  text += "Content-type: text/plain; charset=iso-2022-jp\n"
+  text += "Sender: #{from}\n"
+  text += "Date: #{date}\n"
+  text += "To: #{to}\n"
+  text += "\n\n"
+  text += "#{msg}\n"
+  text += "-----end of message---------------------\n"
+
+  begin
+    STDERR.print "start smtp...\n"
+    smtp = Net::SMTP.start( "localhost" , 25 )
+
+    STDERR.print "send_mail:"
+    smtp.send_mail( text, from, to )
+    smtp.finish
+    STDERR.print "finished.\n"
+    return true
+  rescue
+    STDERR.print "SEND ERROR : #{$!}\n"
+    STDERR.print "mail text:\n"
+    STDERR.print text
+    return false
+  end
+end
+
+def main(argv)
+  if argv.size != 3 then
+    STDERR.print "arg: FROM TO MSG\n"
+    return 
+  end
+  send( argv[0], argv[1], argv[2] )
+end
+
+
+main(ARGV)
+
 
