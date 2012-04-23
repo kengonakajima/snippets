fd3f52264b551b9b28cbbadb63ef82f6772e240c
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-04-24 06:17:09 +0900

    trace

--- ruby/ex/e.rb
+++ ruby/ex/e.rb
@@ -6,12 +6,12 @@ begin
   begin
     aho()
   rescue
-    print "hoge 1\n"
-    raise $!
+    print "hoge 1 #{$!.backtrace}\n"
+    raise $!.backtrace
   end
 rescue
   print "hoge 2 #{$!}\n"
-  print $!,"\n"
+  print $!.backtrace,"\n"
 end
 
 # ->
