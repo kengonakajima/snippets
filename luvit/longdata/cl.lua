9a49c7e55ad1455a85e86099ef045ca75c2368cf
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-05-24 07:27:00 +0900

    luvit sends long data stable

--- luvit/longdata/cl.lua
+++ luvit/longdata/cl.lua
@@ -1,13 +1,39 @@
 local net = require('net')
+local io = require("io")
+local string = require("string")
 
-local client
-client = net.createConnection(61111, '127.0.0.1', function (err)
+local cl
+cl = net.createConnection(61111, '127.0.0.1', function (err)
   if err then error(err) end
   print("Connected...")
-  client:on("data",function(data)
-      print("data:",#data)
-      client:emit("data", "hoge\n")
-      print("hog")      
+  cl:write("hi\n")
+
+  cl.cnt = 1
+  
+  cl:on("data",function(data)
+      assert(data)
+      assert(#data > 0 )
+      for i=1,#data do
+        local ch = string.byte(data,i,i)
+        if ch == ( cl.cnt % 32 ) then
+          if (cl.cnt%100000)==0 then
+            io.stderr:write( "o")
+          end
+        else
+          if ch == string.byte("r",1,1) then
+            cl.cnt = 0
+            io.stderr:write( "RST")
+          elseif ch == string.byte("q",1,1) then
+            print("test done. cnt:", cl.cnt)
+            process.exit(0)
+          else
+            io.stderr:write( "e("..cl.cnt..":" .. ch ..")")
+          end          
+        end
+
+        
+        cl.cnt = cl.cnt + 1
+      end      
     end)
 
 end)
--- luvit/longdata/sv.lua
+++ luvit/longdata/sv.lua
@@ -1,12 +1,37 @@
 local net = require('net')
+local io = require("io")
+local string = require("string")
+local table = require("table")
+
+function mkstr(n)
+  local t = {}
+  for i=1,n do
+    table.insert(t, string.char(i%32) )
+  end
+  return table.concat( t,"")
+end
+
+local s1000 = mkstr(1000)
+local s10000 = mkstr(10000)
+local s100000 = mkstr(100000)
+local s1000000 = mkstr(1000000)
 
 local server
-server = net.createServer(function (client)
+server = net.createServer(function (cl)
+    cl:on("error", function(e)
+         io.stderr:write("E") 
+      end)
 
-    client:on("data", function(data)
+    cl:on("data", function(data)
         print("data:",#data)
-        client:emit("data", "hello\n")
-        print("hel")
+        io.stderr:write(".")
+
+        for i=1,1000 do
+          cl:write(s1000000)
+          cl:write("r")
+        end
+
+        cl:write("q")
       end)
     
     
