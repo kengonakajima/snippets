7db029be2b7dda3668de612a2956436df609e3bf
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-05-24 06:30:57 +0900

    rename

---
+++ luvit/httplong.lua
@@ -0,0 +1,21 @@
+local table = require("table")
+
+local http = require("http")
+
+local ary = {}
+local len = 10000000
+for i=1,len do
+  if (i%100)==0 then ary[i] = "\n" else ary[i] = "." end  
+end
+ary[len+1] = "END"
+local longstr = table.concat(ary)
+
+local sv = http.createServer(function (req, res)
+  res:writeHead(200, {
+    ["Content-Type"] = "text/plain",
+    ["Content-Length"] = #longstr
+  })
+  res:finish(longstr)
+end):listen(8080)
+
+print("Server listening at http://localhost:8080/")
