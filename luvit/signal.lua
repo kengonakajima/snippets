821f0141be13c6ac83410ecffc2de23b9f99a835
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-06-04 06:23:33 +0900

    kill

--- luvit/signal.lua
+++ luvit/signal.lua
@@ -3,6 +3,8 @@ local ffi = require("ffi")
 
 ffi.cdef[[
     void (*signal(int sig, void (*func)(int)))(int);
+    int kill(int pid, int sig);
+    int getpid();
 ]]
 
 ffi.C.signal( 2, function(n)
@@ -15,5 +17,7 @@ server = net.createServer(function (client)
   client:pipe(client)
   client:pipe(process.stdout)
 end):listen(8080)
+
+ffi.C.kill( ffi.C.getpid(), 2 )
 
 print("TCP echo server listening on port 8080")
