1e0fb41e20bf8c2e618db97ef66068fd91e0fc58
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-06-12 06:24:39 +0900

    arglog

--- ruby/local/hoge.rb
+++ ruby/local/hoge.rb
@@ -1,3 +1,8 @@
+def arglog(b)
+  b.eval( "local_variables.each do |name| print name, ':',  (eval name), '\t' end")
+  print "\n"
+end
+
 def hoge(a,b,c)
   print local_variables.join(","),"\n"
   local_variables.each do |name|
@@ -5,4 +10,11 @@ def hoge(a,b,c)
   end
 end
 
+def piyo(a,b,c)
+  arglog(binding)
+end
+
+
 hoge(1,2,3)
+
+piyo(4,5,6)
