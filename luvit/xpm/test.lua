413fac4c5d5d4400f5381633273a25318db45125
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-08-01 14:08:38 +0900

    256colors

--- luvit/xpm/test.lua
+++ luvit/xpm/test.lua
@@ -9,9 +9,6 @@ function XPMImage(w,h)
   function img:addColor(r,g,b)
     local s = string.format( "#%02x%02x%02x", r,g,b)
     table.insert( self.colors, s )
-    if #self.colors > 52 then
-      error("too many colors")
-    end
     return #img.colors    
   end
   
@@ -24,18 +21,16 @@ function XPMImage(w,h)
     row[ix] = colorid
   end
 
+  function img:col2str(id)
+    return string.format("%02x",id)
+  end
+  
   function img:out(name)
     
     local outary = {}
-
-    local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    local charary = {}
-    for i=1,#chars do
-      table.insert( charary, chars:sub(i,i) )
-    end
     
     for i,v in ipairs(self.colors) do
-      table.insert( outary, string.format( "\"%s c %s\",\n", charary[i], self.colors[i] ) )
+      table.insert( outary, string.format( "\"%s c %s\",\n", self:col2str(i), self.colors[i] ) )
     end
       
     for y=0,self.width-1 do
@@ -43,21 +38,21 @@ function XPMImage(w,h)
       local rowstrary = {}
       for x=0,self.height-1 do
         if not row then
-          table.insert( rowstrary, "." )
+          table.insert( rowstrary, "<>" )
         else
           local pixel = row[x+1]
           if pixel then
-            table.insert( rowstrary, charary[pixel] )
+            table.insert( rowstrary, self:col2str(pixel))
           else
-            table.insert( rowstrary, "." )
+            table.insert( rowstrary, "<>" )
           end
         end
       end
       table.insert( outary, "\"" .. table.concat( rowstrary ) .. "\",\n" )
     end
     return  "static const char* const " .. name .. "[] = {\n" ..
-      string.format( "\"%d %d %d %d\",\n", self.width, self.height, (#self.colors+1), 1 ) ..
-      "\". c None\",\n" .. 
+      string.format( "\"%d %d %d %d\",\n", self.width, self.height, (#self.colors+1), 2 ) ..
+      "\"<> c None\",\n" .. 
       table.concat( outary ) .. "};\n"
   end
   
