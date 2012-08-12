5e06a262b5cdd6cbe6808e5abc714231645f13ab
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-08-13 07:47:11 +0900

    hitcpp

--- ispc/hit.cpp
+++ ispc/hit.cpp
@@ -1,19 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-static const int N=1024*1024;
+
+class Point
+{
+public:
+    float x,y;
+    void randomize() {
+        x = (random() % 100)/100.0f;
+        y = (random() % 100)/100.0f;
+    }
+    Point* hit(Point*points, int n) {
+        for(int i=0;i<n;i++){
+            Point *tgt = & points[i];
+            float d=4;
+            if( this->x+d > tgt->x-d && this->x-d < tgt->x+d && this->y+d > tgt->y-d && this->y-d < tgt->y+d ) {
+                return tgt;
+            }
+        }
+        return NULL;
+    }
+};
 
 
-float vin[N];
+void hitall( Point*points, Point**hits, int n ) {
+    int i;
+    for(i=0;i<n;i++){
+        Point *p = & points[i];
+        Point *hitpt = p->hit(points,n);
+        hits[i] = hitpt;
+    }
+}
+                                                 
+          
+
+void doit(int n ) {
+    Point *points = (Point*)malloc( sizeof(Point)*n);
+    for(int i=0;i<n;i++){
+        points[i].randomize();
+    }
+    Point **hits = (Point**)malloc( sizeof(Point*)*n);
+    for(int i=0;i<n;i++){
+        hits[i] = NULL;
+    }
+
+    hitall( points, hits, n );
+    int hitcnt=0;
+    for(int i=0;i<n;i++){
+        if(hits[i]){hitcnt++;}
+    }
+    printf("hitcnt:%d\n", hitcnt );
+    free(points);
+    free(hits);
+}
+
+
           
 int main() {
-    float tot=0;
-    for(int i=0;i<N;i++){
-        vin[i] = (float)i;
-    }
-
-    for(int i=0;i<N;i++){
-        tot += vin[i];
-    }
-    printf("tot:%f\n", tot );
-    return 0;    
+    doit(1024*1024*32);
+    return 0;
 }
