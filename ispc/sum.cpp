3a78b2ce75cc9c9d2147fb2e18d691a8a9a07761
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-08-13 06:53:39 +0900

    do not repeat inits

--- ispc/sum.cpp
+++ ispc/sum.cpp
@@ -2,27 +2,30 @@
 #include <stdlib.h>
 
 
+float fsum( float *vin, int n ) {
+    float tot=0;
+    for(int i=0;i<n;i++){
+        tot += vin[i];
+    }
+    return tot;    
+}
 
 
 void doit(int n ) {
     float *vin = (float*)malloc( sizeof(float)*n);
-    float tot=0;
     for(int i=0;i<n;i++){
         vin[i] = (float)i;
     }
-
-    for(int i=0;i<n;i++){
-        tot += vin[i];
+    for(int i=0;i<10;i++){    
+        float out = fsum(vin, n);
+        printf("tot:%f iter:%d\n", out,i );
     }
-    printf("tot:%f\n", tot );
     free(vin);
 }
 
 
           
 int main() {
-    for(int i=0;i<10;i++){
-        doit(1024*1024*32);
-    }
+    doit(1024*1024*32);
     return 0;
 }
