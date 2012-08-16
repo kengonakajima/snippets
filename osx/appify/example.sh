edec610dbdceec8546905db85762355349339928
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-08-16 09:54:51 +0900

    get user's dir

--- osx/appify/example.sh
+++ osx/appify/example.sh
@@ -2,3 +2,12 @@
 
 echo Hello world!
 open /
+pwd > /tmp/appify_test_out1
+ps > /tmp/appify_test_out2
+whoami > /tmp/appify_test_out3
+echo ~ > /tmp/appify_test_out4
+ls ~ > /tmp/appify_test_out5
+ls ~/Library > /tmp/appify_test_out6
+
+
+
