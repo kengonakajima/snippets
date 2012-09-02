45dc08b8bf1358d6d7d975d86df757079af233f9
Kengo Nakajima Deploy User <ken.go.nakajima@gmail.com>
2012-09-02 14:07:32 +0900

    reading burrito

--- node/burrito.js
+++ node/burrito.js
@@ -13,14 +13,41 @@ var burrito = require("burrito");
 
 var fs =require("fs");
 
+function spaces(depth) {
+    var out="";
+    for(var i=0;i<depth;i++){
+        out += " ";
+    }
+    return out;
+}
 
-fs.readFile( "./jquery.js", function(err,data) {
+function traverse(ast,depth) {
+    for(var i=0;i<ast.length;i++){
+        var node = ast[i];
+        if( typeof(node)== "object" ){
+            traverse(node,depth+1);
+        } else {
+            if( node.length == undefined ){
+                console.log( spaces(depth), "NODE:", node );
+            } else {
+                console.log( spaces(depth), "NODEN:", node.length );
+                traverse(node,depth+1);
+            }
+        }
+    }
+}
+
+fs.readFile( "./t.js", function(err,data) {
                  console.log("data:",data);
+                 var ast = burrito.parse(data.toString(),false,true);
+         //        traverse(ast,0);
+                 console.log(JSON.stringify(ast,null,1));
+
+                 
                  var res = burrito(data, function(node) {
-                                       console.log("NODE:",node.name );
+                                       console.log("NODE:",node.name, "VAL:", node.value, "PAR:", node.parent() );
                                        if( node.name == "call" ) node.wrap( "AHO(%s)" );
                                    });
-
                  console.log("res:",res);
                  
              });
---
+++ node/t.js
@@ -0,0 +1,16 @@
+// aho
+var a=1;
+/*
+ var b=2+3;
+var c=a+b;
+6+7;
+
+console.log("hello");
+
+function a() {
+}
+
+function b() {
+    a();
+}
+*/
