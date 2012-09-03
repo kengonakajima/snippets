218fb9725acf4c5a7fa590cbdc2ffe0129480213
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-09-03 11:44:54 +0900

    1500 sprite, 1deck:60fps 100deck=52fps

--- moai/ndeck/main.lua
+++ moai/ndeck/main.lua
@@ -16,33 +16,58 @@ layer:setViewport(viewport)
 
 Sim.pushRenderPass(layer)
 
-deck = loadTileDeck( "minecraft.png", 16,16, false, false )
-assert(deck)
+-------------------------------
 
-n = 20000
-dur = 30
+ndeck = 300
+n = 1500
+dur = 10
+
+-- deck=300, n=1500 : 50fps @mbp
+-- deck=100, n=1500 : 52fps @mbp
+-- deck=1, n=1500 : 60fps @mbp
+-------------------------------
+
+decks = {}
+for i=1,ndeck do
+  local dk = loadTileDeck( "minecraft.png", 16,16, false, false )
+  assert(dk)
+  insert(decks,dk)
+end
+
+
+
 
 for i=1,n do
-  local x,y = range(-SCRW,SCRW), range(-SCRH,SCRH)
+  local x,y = range(-SCRW,SCRW)/2, range(-SCRH,SCRH)/2
   p = Prop2D.new()
-  p:setDeck(deck)
-  p:setIndex(irange(1,96) )
-  local r = range(1,32)
-  p:setScl(r,r)
+  p:setDeck( choose(decks) )
+  if range(0,1) < 0.5 then
+    local g = Grid.new()
+    local gs = 8
+    g:setSize(gs,gs,16,16,0,0,16,16)
+    p:setGrid(g)
+    ii=1
+    for x=1,gs do
+      for y=1,gs do
+        g:setTile(x,y,ii%96)
+        ii = ii + 1
+      end
+    end
+    p:setScl(0.5,0.5)
+  else
+    p:setIndex(irange(1,96) )
+    local r = range(1,32)
+    p:setScl(r,r)
+  end  
   p:setLoc(x,y)
-  p:seekLoc(0,0,dur,EaseType.LINEAR )
+  p:seekLoc(range(-SCRW,SCRW)/2,range(-SCRH,SCRH)/2,dur,EaseType.LINEAR )
   p:seekRot(dur/2*360,range(dur,dur*2), EaseType.LINEAR )
 
   layer:insertProp(p)
 end
 
 every(1,function()
-        local f = Sim.getElapsedFrames()        
-        if not prevFrame then prevFrame = f end
-        local df = f - prevFrame
-        prevFrame = f
-        
-        print( "fps:", df )
+        print( "fps:", Sim.getPerformance())
         end)
 
 
