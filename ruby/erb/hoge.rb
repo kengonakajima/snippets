02402647811849fe04191b6978a3e35680b32d65
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-12-01 17:49:48 +0900

    sinatra

---
+++ ruby/erb/hoge.rb
@@ -0,0 +1,8 @@
+require "../rumino/rumino"
+
+
+a = 123
+b = 234
+
+o = doerb( "hoge.tmpl", binding )
+print o, "\n"
---
+++ ruby/sinatra/hoge.rb
@@ -0,0 +1,29 @@
+require "rubygems"
+require "sinatra"
+require "erb"
+require "json"
+
+helpers do
+  def mul(x)
+    x * 2
+  end
+end
+
+get "/" do
+  "<html><head><title>xx</title><body>hello world</body></html>"
+end
+
+get "/snippet/:name" do
+  n = params[:name]
+  "snippet name:#{n} #{n} #{n}"
+end
+
+
+get "/poo" do
+  @poo = 1234
+  erb :t, :locals => { :localpoo => 23456, :agent => request.user_agent, :lang => request.env["HTTP_ACCEPT_LANGUAGE"] }
+end
+
+not_found do
+  "not found!!!!!!!!!!"
+end
