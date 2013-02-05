9ef6931abc0cdb22a7f035e4b9af917589539733
Kengo Nakajima <kengo.nakajima@gmail.com>
2013-02-05 15:19:47 +0900

    de

--- cpp/destr.cpp
+++ cpp/destr.cpp
@@ -13,9 +13,28 @@ class B : public A
     ~B(){printf("b\n");};
 };
 
+class C
+{
+public:
+    virtual ~C(){printf("c\n");}
+};
+class D : public C
+{
+public:
+     ~D(){printf("d\n"); }
+};
+
 int main()
 {
     B *b = new B();
 
     delete b;
+
+    B *b2 = new B();
+    A *a = (A*)b2;
+    delete a;
+
+    D *d = new D();
+    C *c = (C*) d;
+    delete c;
 }
