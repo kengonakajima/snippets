9a483f3bb9b9356138ff71d01d28f9ed9351fe4e
Kengo Nakajima <kengo.nakajima@gmail.com>
2013-12-31 01:05:03 +0900

    small

--- cpp/hashmap/hoge.cpp
+++ cpp/hashmap/hoge.cpp
@@ -10,6 +10,16 @@ int gen() {
     return random() & 0xffff;
 }
 
+// 10M, O3: 0.04s
+void testEmpty() {
+    long long tot=0;
+    int n = 10000000;
+    for(int i=0;i<n;i++) {
+        int r = gen();
+        tot += r;
+    }
+    cerr << "TOT:" << tot << endl;    
+}
 
 // 10M, O3 : 3s sys 99%   init, searchが同じぐらいの時間かかる。
 void testMap() {
@@ -45,19 +55,30 @@ void testUnorderedMap() {
     }
     cerr << "TOT:" << tot << endl;    
 }
-// 10M, O3: 0.04s
-void testEmpty() {
-    long long tot=0;
-    int n = 10000000;
+
+// 10M, O3 : 0.35s sys 99%
+void testUnorderedSmallMap() {
+    unordered_map<int,int> u;
+    int n = 10000;
+    int loop = 10000000;
+    cerr << "init" << std::endl;
     for(int i=0;i<n;i++) {
         int r = gen();
-        tot += r;
+        u[r] = i;
+    }
+    cerr << "search" << std::endl;
+    long long tot=0;
+    for(int i=0;i<loop;i++) {
+        int r = gen();
+        tot += u[r];
     }
     cerr << "TOT:" << tot << endl;    
 }
 
+
 int main() {
     //    testMap();
-        testUnorderedMap();
+    //    testUnorderedMap();
+    testUnorderedSmallMap();
     // testEmpty();
 }
