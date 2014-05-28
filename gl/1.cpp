eee8372b5b2f2d529358ffa21d1e1cea3352f02a
Kengo Nakajima <kengo.nakajima@gmail.com>
2014-05-28 10:37:12 +0900

    indent

--- gl/1.cpp
+++ gl/1.cpp
@@ -16,12 +16,12 @@ void disp( void ) {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_POLYGON);
-		glColor3f(1 , 0 , 0);
-		glVertex2f(-0.9 , -0.9);
-		glColor3f(0 , 1 , 0);
-		glVertex2f(0 , 0.9);
-		glColor3f(0 , 0 , 1);
-		glVertex2f(0.9 , -0.9);
+    glColor3f(1 , 0 , 0);
+    glVertex2f(-0.9 , -0.9);
+    glColor3f(0 , 1 , 0);
+    glVertex2f(0 , 0.9);
+    glColor3f(0 , 0 , 1);
+    glVertex2f(0.9 , -0.9);
 	glEnd();
 
 	glFlush();
