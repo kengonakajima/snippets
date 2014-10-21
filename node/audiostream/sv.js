84761f74f49d3917e47fa7182ccbb8975fc0951a
Kengo Nakajima <kengo.nakajima@gmail.com>
2014-10-22 05:32:21 +0900

    add jquery

--- node/audiostream/sv.js
+++ node/audiostream/sv.js
@@ -2,8 +2,15 @@ var pcm = require('pcm');
 
 var wss = new (require('ws').Server)({
     server: require('http').createServer(function(req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end(require('fs').readFileSync('index.html'));
+        console.log("http req.url:",req.url);
+        
+        if( req.url == "/jquery.js" ) {
+            res.writeHead(200, { 'Content-Type': 'text/javascript'});
+            res.end(require('fs').readFileSync('jquery.js'));            
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.end(require('fs').readFileSync('index.html'));
+        }
     }).listen(8888)
 });
 
@@ -13,17 +20,23 @@ var idx = 0;
 wss.on('connection', function (ws) {
     console.log('connected');
     // モノラル、44.1kHz
-    pcm.getPcmData('nobara_hayakawa_trail_cc.mp3', { stereo: false, sampleRate: 44100 },
-        function(sample, channel) {
-            console.log("data! ", idx );
+    pcm.getPcmData('nobara_hayakawa_trail_cc.mp3',
+                   { stereo: false, sampleRate: 44100 },
+                   function(sample, channel) {
+//                       console.log("data! ", idx );
 
-            buf[idx++] = sample;
-            // 適当に8192サンプルずつで区切って送信する
-            if (idx == buf.length) {
-                ws.send(buf);
-                idx = 0;
-            }
-        }
+                       buf[idx++] = sample;
+                       // 適当に8192サンプルずつで区切って送信する
+                       if (idx == buf.length) {
+                           ws.send(buf);
+                           idx = 0;
+                       }
+                   },
+                   function(a,b) {
+                       console.log("endCallback a:",a);
+                       console.log("endCallback b:",b);
+                   }
+                   
     );
 
     ws.on('close', function () {
