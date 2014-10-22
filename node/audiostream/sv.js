a809b610273c09daef45bb34292035b3b366223a
Kengo Nakajima <kengo.nakajima@gmail.com>
2014-10-22 09:36:54 +0900

    mixer

--- node/audiostream/sv.js
+++ node/audiostream/sv.js
@@ -14,11 +14,60 @@ var wss = new (require('ws').Server)({
     }).listen(8888)
 });
 
-var buf = new Float32Array(8192);
-var idx = 0;
+var bgmbuf = new Float32Array(1024*1024);
+var bgmidx = 0;
 
-var blllbuf = new Float32Array(1024*1024);
+var blllbuf = new Float32Array(16384);
 var blllidx = 0;
+
+var mixlen = 16384;
+var mixbuf = new Float32Array(mixlen);
+
+// 送信用のバッファとmix用のバッファは別なので、常にバッファの先頭から書き込むのでOK
+// mixlenより長い音は再生できない
+function play(buf) {
+    console.log( "play: len:", buf.length );
+    
+    var i;
+    for(i=0;i<buf.length && i < mixlen ;i++) {
+        mixbuf[i] += buf[i];
+    }
+}
+// 長さの分だけシフトしてバッファを返す
+function shiftMix(len) {
+    var out = new Float32Array(len)
+    var i;
+    for(i=0;i<len && i < mixlen;i++) {
+        out[i] = mixbuf[i];
+    }
+    for(i=len;i<mixlen;i++) {
+        mixbuf[i-len] = mixbuf[i];
+    }
+    for(i=mixlen-len;i<mixlen;i++) {
+        mixbuf[i] = 0;
+    }
+    return out;
+}
+
+var interval_ms = 16.666;
+var hz = 44100;
+var sample_per_sec = hz * 1;
+var sample_per_ms = sample_per_sec / 1000.0;
+var sample_per_cb = parseInt(sample_per_ms * interval_ms);
+console.log( "Sample per callback:", sample_per_cb );
+
+var connections = new Array();
+
+function mixSenderCallback() {
+    var to_send = shiftMix(sample_per_cb);
+    if( connections.length > 0 ) console.log( "sending samples:", sample_per_cb, "conns:", connections.length, to_send[0] );    
+    var i;
+    for(i=0;i<connections.length;i++) {
+        var c = connections[i];
+        c.send(to_send);
+    }
+}
+setInterval( mixSenderCallback, interval_ms );
 
 pcm.getPcmData( "blll.wav",
                 { stereo:false, sampleRate:44100 },
@@ -26,38 +75,48 @@ pcm.getPcmData( "blll.wav",
                     blllbuf[blllidx++] = sample;
                 },
                 function(a,b) {
-                    console.log("bllll load finished. size:", blllidx);
+                    console.log("bllll load finished. size:", blllbuf.length );
                 }
               );
                 
 
+// モノラル、44.1kHz
+pcm.getPcmData('nobara_hayakawa_trail_cc.mp3',
+               { stereo: false, sampleRate: 44100 },
+               function(sample, channel) {
+                   bgmbuf[bgmidx++] = sample;
+               },
+               function(a,b) {
+                   console.log("bgm load finished. size:", bgmbuf.length );
+               }
+              );
+
+
+function regConnection(c) {
+    connections.push(c);
+}
+function delConnection(c) {
+    var at = connections.indexOf(c);
+    connections.splice(at,1);
+}
 
 wss.on('connection', function (ws) {
     console.log('connected');
-    // モノラル、44.1kHz
-    pcm.getPcmData('nobara_hayakawa_trail_cc.mp3',
-                   { stereo: false, sampleRate: 44100 },
-                   function(sample, channel) {
-//                       console.log("data! ", idx );
-
-                       buf[idx++] = sample;
-                       // 適当に8192サンプルずつで区切って送信する
-                       if (idx == buf.length) {
-                           ws.send(buf);
-                           idx = 0;
-                       }
-                   },
-                   function(a,b) {
-                       console.log("endCallback a:",a);
-                       console.log("endCallback b:",b);
-                   }
-                   
-    );
-
+    regConnection(ws);
+    
     ws.on('close', function () {
         console.log('close');
+        delConnection(ws);
     });
     ws.on("message", function(data) {
         console.log("data:",data);
+        
+        if( data == 65 ) { // a
+            play(blllbuf);            
+        }
+        if( data == 66 ) {
+            var i;
+            for(i=0;i<1000;i++) console.log( mixbuf[i] );
+        }
     });
 });
