9cfe1604379137a20f2a33218ba606a2cb62537b
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-11-07 15:56:45 +0900

    pidfile

---
+++ ruby/pidfile/hoge.rb
@@ -0,0 +1,23 @@
+
+require "pidfile"
+
+if PidFile.running?
+  print "running!\n"
+  exit
+end
+
+print "not running, start!\n"
+
+p = PidFile.new
+
+puts p.pidfile
+puts p.piddir
+puts p.pid
+puts p.pidpath
+
+
+10.times do |i|
+  sleep 1
+  print i, "\n"
+end
+
