3775a488b23684142201633269bf18cf52297eec
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-12-21 13:26:17 +0900

    endian

--- c/endian/hoge.c
+++ c/endian/hoge.c
@@ -4,5 +4,8 @@ int main() {
     long long x = 0x0102030405060708;
     char *p = (char*)&x;
     printf("%lld\n",x);
-    printf("%02x %02x %02x %02x %02x %02x %02x %02x", p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7] );    
+    printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7] );
+
+    long long y = 0x0000010203040506;
+    printf("%lld\n",y);
 }
