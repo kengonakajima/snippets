3e921511185e291da1e69e186e745d6607c5b970
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-01-16 10:12:33 +0900

    getvalue

--- js/em/test.js
+++ js/em/test.js
@@ -55,8 +55,18 @@ var input_heap = makeI8Heap(g,input_data);
 var output_heap = makeI8Heap(g,new Int8Array(10));
 var outputlen_heap = makeI8Heap(g,new Int8Array([10,0,0,0]));
 
+console.log("BOs: input:", input_heap.byteOffset, "output_heap:", output_heap.byteOffset, "outputlen_heap:", outputlen_heap.byteOffset );
+
+for(var i=0;i<10;i++)  console.log("input_heap data :", i, " : ", g.getValue(input_heap.byteOffset+i,"i8"));
+for(var i=0;i<10;i++)  console.log("output_heap data :", i, " : ", g.getValue(output_heap.byteOffset+i,"i8"));
+for(var i=0;i<10;i++)  console.log("outputlen_heap data :", i, " : ", g.getValue(outputlen_heap.byteOffset+i,"i8"));
+
+
 var ccc = g.cwrap("ccc","number",["number","number","number","number"]);
 var r = ccc(input_heap.byteOffset,10,output_heap.byteOffset,outputlen_heap.byteOffset);
 output = new Int8Array(output_heap.buffer,output_heap.byteOffset,10);
 var outputlen = new Int8Array(outputlen_heap.buffer,outputlen_heap.byteOffset,4);
 console.log("ccc result:",r,"output:",output,"outputlen:",outputlen);
+
+// heap access test
+
