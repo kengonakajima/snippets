6af8dcbdfd28d3d94fcd4d1a86c83ac1919e1b6e
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-02-01 06:00:06 +0900

    delay

--- node/png/hoge.js
+++ node/png/hoge.js
@@ -3,14 +3,22 @@ console.log(PNG);
 var sol_u8a=require("./sol.js");
 console.log(sol_u8a);
 
-
+var out;
+console.log("start..");
 var png = new PNG(sol_u8a);
 png.decode(function(pixels) {
-    console.log(pixels.length,pixels);
-    for(var i in pixels) {
-
-    }
-        
+    console.log("pixels",pixels.length,pixels,new Date().getTime());
+    out=pixels;
 });
+setTimeout( function() {
+console.log("out p2",out,new Date().getTime());    
+}, 50)
 
+console.log(new Date().getTime())
+
+console.log("out p1",out,new Date().getTime());
+
+
+
+        
 
