c77a24ba7bda545823adb20a7eb2aa3dcc798383
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-02-10 05:44:39 +0900

    node

--- node/pngjs2/hoge.js
+++ node/pngjs2/hoge.js
@@ -1,8 +1,11 @@
+console.log("Buffer:",Buffer);
 var PNG=require("pngjs2").PNG;
+console.log("Buffer:",Buffer);
 console.log(PNG);
 var sol_u8a=require("./sol.js");
 console.log(sol_u8a);
 var buf = new Buffer(sol_u8a);
+console.log(buf);
 var out = new PNG.sync.read(buf);
 console.log("output:",out);
 
