d5c5e2aa0b2a7fdc81286bfe8794b687cdf08f28
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-02-28 06:35:23 +0900

    sh2

--- js/three/shader.js
+++ js/three/shader.js
@@ -1,3 +1,5 @@
+// http://qiita.com/_nabe/items/1657d9556591284a43c8
+
 var renderer;
 var cameraOrtho, sceneOrtho;
 
@@ -6,16 +8,77 @@ var vertex_glsl =
     "gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);\n" +
     "}";
 
+var vertex_glsl_2 =
+    "varying vec2 vUv;// fragmentShaderに渡すためのvarying変数\n"+
+    "void main()\n"+
+    "{\n"+
+    "  // 処理する頂点ごとのuv(テクスチャ)座標をそのままfragmentShaderに横流しする\n"+
+    "  vUv = uv;\n"+
+    "  // 変換：ローカル座標 → 配置 → カメラ座標\n"+
+    "  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);\n"+
+    "  // 変換：カメラ座標 → 画面座標\n"+
+    "  gl_Position = projectionMatrix * mvPosition;\n"+
+    "}\n";
+
+var vertex_glsl_3 = 
+	"uniform float time;\n"+
+	"uniform vec2 resolution;\n"+
+	"void main()	{\n"+
+	"	gl_Position = vec4( position, 1.0 );\n"+
+	"}\n";
+
+
 var fragment_glsl =
     "void main(void) {\n" +
     "gl_FragColor = vec4(1.0, .0, .0, 1.);\n"+
     "}";
 
+var fragment_glsl_2 =
+    "//uniform 変数としてテクスチャのデータを受け取る\n"+
+    "uniform sampler2D texture;\n"+
+    "// vertexShaderで処理されて渡されるテクスチャ座標\n"+
+    "varying vec2 vUv;\n"+
+    "void main()\n"+
+    "{\n"+
+    "  // テクスチャの色情報をそのままピクセルに塗る\n"+
+    "  gl_FragColor = texture2D(texture, vUv);\n"+
+    "}\n";
+
+var fragment_glsl_3 = 
+	"uniform float time;\n"+
+	"uniform vec2 resolution;\n"+
+	"void main()	{\n"+
+	"	float x = mod(time + gl_FragCoord.x, 20.) < 10. ? 1. : 0.;\n"+
+	"	float y = mod(time + gl_FragCoord.y, 20.) < 10. ? 1. : 0.;\n"+
+	"	gl_FragColor = vec4(vec3(min(x, y)), 1.);\n"+
+	"}\n";
+
+
+var replacer_glsl = 
+	"uniform sampler2D texture;\n"+
+	"uniform vec3 color1;\n"+
+	"uniform vec3 replace1;\n"+
+	"uniform float eps;\n"+
+	"void main() {\n"+
+	"	vec4 pixel = texture2D(texture, gl_TexCoord[0].xy); \n"+
+	"	if(pixel.r > color1.r - eps && pixel.r < color1.r + eps && pixel.g > color1.g - eps && pixel.g < color1.g + eps && pixel.b > color1.b - eps && pixel.b < color1.b + eps ){\n"+
+	"		pixel = vec4(replace1, pixel.a );\n"+
+	"    }\n"+
+	"   pixel.r = gl_Color.r * pixel.r;\n"+
+	"   pixel.g = gl_Color.g * pixel.g;\n"+
+	"   pixel.b = gl_Color.b * pixel.b;\n"+
+	"   pixel.a = gl_Color.a * pixel.a;\n" +   
+	"	gl_FragColor = pixel;\n"+
+	"}\n";
+
+
+
+
 init();
 animate();
 
 
-
+var uniforms_3;
 
 function init() {
 
@@ -60,11 +123,45 @@ function createRectGeometry(width,height) {
 }
 
 function createHUDSprites ( texture ) {
+/*
+void ColorReplacerShader::updateUniforms(){
+	float fromcol[]={ from_color.r, from_color.g, from_color.b};
+	GLuint uniid1=glGetUniformLocation(program,"color1");
+	glUniform3fv( uniid1,1,fromcol);
+
+	float tocol[]={ to_color.r, to_color.g, to_color.b };
+	GLuint uniid2=glGetUniformLocation(program,"replace1");
+	glUniform3fv( uniid2,1,tocol);
+
+	GLuint uniid3=glGetUniformLocation(program,"texture");
+	glUniform1i(uniid3, 0 );
+
+	GLuint uniid4=glGetUniformLocation(program,"eps");
+	glUniform1f(uniid4, epsilon );
+}
+*/
+
     uniforms={}
+    uniforms_1={
+        "texture" : { type: "t", value: texture }
+    };
+    uniforms_2={
+        "texture" : { type: "t", value: texture },        
+        "color1" : { type: "v3", value: new THREE.Vector3(1,0,0) },
+        "replace1" : { type: "v3", value: new THREE.Vector3(0,1,0) },
+        "eps" : { type: "f", value: 0.1 }
+    };
+    uniforms_3={
+		time: { type: "f", value: 1.0 },
+		resolution: { type: "v2", value: new THREE.Vector2() }        
+    };
+	uniforms_3.resolution.value.x = window.innerWidth;
+	uniforms_3.resolution.value.y = window.innerHeight;
+    
     var material = new THREE.ShaderMaterial( {
-        uniforms : uniforms,
-        vertexShader : vertex_glsl,
-        fragmentShader : fragment_glsl
+        uniforms : uniforms_1,
+        vertexShader : vertex_glsl_3,
+        fragmentShader : fragment_glsl_3
     });
 //    var material = new THREE.SpriteMaterial( { map: texture } );
 	var geom = createRectGeometry(1,1);
@@ -73,8 +170,12 @@ function createHUDSprites ( texture ) {
 	mesh.position.set(0,0,1);
 	sceneOrtho.add(mesh);
 }
-
+var startTime = Date.now();
 function animate() {
+	var elapsedMilliseconds = Date.now() - startTime;
+	var elapsedSeconds = elapsedMilliseconds / 1000.;
+	if(uniforms_3) uniforms_3.time.value = 60. * elapsedSeconds;
+    
 	requestAnimationFrame( animate );
 	render();
 
