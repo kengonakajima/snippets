aaa71448f0030ec9017917cbfe47d16f1ff83876
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-05-27 10:27:31 +0900

    Profile

--- node/mojang/hoge.js
+++ node/mojang/hoge.js
@@ -2,8 +2,16 @@ var MojangAPI = require('mojang-api');
 var date = new Date();
 date.setMonth(0); // 0 = January
 MojangAPI.uuidAt('jeb_', date, function(err, res) {
-    if (err)
+    if (err) {
         console.log(err);
-        else
-            console.log("On " + date + ", jeb_'s UUID was " + res.id);
+    } else {
+        console.log("On " + date + ", jeb_'s UUID was " + res.id);
+        MojangAPI.profile(res.id, function(err, res) {
+            if (err)
+                console.log(err);
+            else {
+                console.log(res);
+            }
+        });
+    }
 });
