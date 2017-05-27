8ee1a046b67188a35b7027fe39bd6242e629eef6
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-05-27 10:25:33 +0900

    mojang

---
+++ node/mojang/hoge.js
@@ -0,0 +1,9 @@
+var MojangAPI = require('mojang-api');
+var date = new Date();
+date.setMonth(0); // 0 = January
+MojangAPI.uuidAt('jeb_', date, function(err, res) {
+    if (err)
+        console.log(err);
+        else
+            console.log("On " + date + ", jeb_'s UUID was " + res.id);
+});
