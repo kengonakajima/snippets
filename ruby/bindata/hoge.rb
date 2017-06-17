c2ec6e2c6e65be2b89d9f0565102b6dbbad3c1b3
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-06-17 17:16:10 +0900

    chunk header

--- ruby/bindata/hoge.rb
+++ ruby/bindata/hoge.rb
@@ -1,8 +1,6 @@
 require "bindata"
 
-data = File.open("r.0.0.mca").read
 
-print "size:", data.bytes.size, "\n"
 
 def findChunkData(data,chx,chz)
   index_in_loctable = (chz%32)*32+(chx%32)
@@ -18,7 +16,35 @@ def findChunkData(data,chx,chz)
   return byte_ofs, size_bytes
 end
 
-ofs,bsz = findChunkData(data,0,0)
+def getU32(data,ofs)
+  b0 = data.bytes[ofs+0]
+  b1 = data.bytes[ofs+1]
+  b2 = data.bytes[ofs+2]
+  b3 = data.bytes[ofs+3]
+  return (b0*65536*256)+(b1*65536)+b2*256+b3
+end
 
+def readChunk(data,ofs,bsz)
+  # 4+1bytes header
+  chunksz = getU32(data,ofs)
+  comptype = data.bytes[ofs+4]
+
+  print "readChunk: chunksz: #{chunksz} comp:#{comptype}\n"
+end
+
+
+#
+#
+#
+
+
+data = File.open("r.0.0.mca").read
+print "size:", data.bytes.size, "\n"
+ofs,bsz = findChunkData(data,0,0)
 print "Chunk 0,0 ofs: at #{ofs},  #{bsz} bytes\n"
+
+
+
+readChunk(data,ofs,bsz)
+
 
