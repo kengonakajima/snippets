6ffc90c8a9c8733ac36bd18b16d7a3eccb13ae1c
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-07-26 15:38:10 +0900

    src and dest mac

--- ruby/pcap/packet.rb
+++ ruby/pcap/packet.rb
@@ -1,12 +1,26 @@
 require "json"
 
+def to_hex(bary,delim)
+  out=[]
+  bary.each do |b| out.push( sprintf("%02x",b) ) end
+  return out.join(delim)
+end
+
 class Packet
   
   def initialize(s)
     @size = s.size
+    @bytes = s.bytes
   end
   def to_s
-    return JSON.generate( { :size => @size } )
+    src_mac = @bytes[0..5]
+    dest_mac = @bytes[6..11]
+    return JSON.generate( {
+      :size => @size,
+      :src_mac => to_hex(src_mac,":"),
+      :dest_mac => to_hex(dest_mac,":")
+      
+      })
   end
   
 end
