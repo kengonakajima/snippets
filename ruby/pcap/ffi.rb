4190370a62bd928ab44e956e795c7c6d61d65e66
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-07-26 14:56:18 +0900

    u-m

--- ruby/pcap/ffi.rb
+++ ruby/pcap/ffi.rb
@@ -9,13 +9,20 @@ pcap = FFI::PCap::Live.new( :dev =>  ARGV[0],
                             :timeout => 1,
                             :promisc => true,
                             :handler => FFI::PCap::Handler )
-
+pcap.nonblocking=true
 pcap.setfilter("tcp")
 
-pcap.loop() do |this, pkt|
-  STDERR.puts "#{pkt.time} : "
-  pkt.body.each_byte {|x| STDERR.print "%0.2x " % x }
-  STDERR.putc "\n"
+
+
+fd = pcap.fileno
+io=IO.new(fd)
+while Kernel.select([io],nil,nil)
+  pcap.dispatch() do |this,pkt|
+    STDERR.puts "#{pkt.time} : "
+    # pkt.body.each_byte {|x| STDERR.print "%0.2x " % x }
+    STDERR.print pkt.header.methods.sort, "\n"
+    STDERR.puts "----"
+  end
 end
 
 
