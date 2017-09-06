fb235ce5392683e579a11a604b6299962e152adf
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-09-06 15:31:27 +0900

    udp echoing

---
+++ node/udp/cl.js
@@ -0,0 +1,57 @@
+var dgram = require('dgram');
+
+var interval_ms = 200;
+var image_size = 20 * 1024;
+var divide_num = 20;
+var bytes_per_message = image_size / divide_num;
+
+var message = "";
+var letters = "abcdefghijkljmnopqrstuvwxyz0123456789";
+for(var i=0; i<bytes_per_message; i++) message += letters[Math.floor(Math.random()*letters.length)];
+
+
+if( bytes_per_message > 1440 ) {
+    console.log("warning: bytes_per_message is larger than 1440: ", bytes_per_message );
+}
+    
+
+var client = dgram.createSocket('udp4');
+
+var host = process.argv[2];
+if(!host) {
+    console.log("need host argument");
+    process.exit(1);
+}
+    
+var port = 22222;
+
+var total_sent_bytes=0;
+var last_sent_bytes=0;
+var total_recv_bytes=0;
+var last_recv_bytes=0;
+
+setInterval( function() {
+    for(var i=0;i<divide_num;i++) {
+        client.send(message, port, host,  function(err, bytes) {
+            if (err) {
+                console.log("send-error, EMSGSIZE means message is larger than MTU");
+                throw err;
+            }
+//            console.log('UDP message sent to ' + host +':'+ port, "len:", bytes);
+            total_sent_bytes += bytes;
+            last_sent_bytes += bytes;
+        });
+    };
+}, interval_ms );
+
+
+client.on("message", function(message,remote){
+    total_recv_bytes += message.length;
+    last_recv_bytes += message.length;
+});
+
+setInterval( function() {
+    console.log( new Date, "Send: ", total_sent_bytes/1000000.0, "MB,(", last_sent_bytes, "B), Recv: ", total_recv_bytes/1000000.0, "MB,(", last_recv_bytes, "B)" );
+    last_sent_bytes = 0;
+    last_recv_bytes = 0;
+}, 1000 );
--- node/udp/sv.js
+++ node/udp/sv.js
@@ -10,10 +10,23 @@ server.on('listening', function () {
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
 });
 
+var total_recv_bytes=0;
+var last_recv_bytes=0;
+
 server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
+//    console.log(remote.address + ':' + remote.port +' - ' + message);
+    server.send(message,0,message.length, remote.port, remote.host, function(err,bytes) {
+//        console.log("sent ", bytes, " bytes to client:", remote.host, remote.port );
+        total_recv_bytes += message.length;
+        last_recv_bytes += message.length;
+    });
 
 });
+
+setInterval( function() {
+    console.log("Recv:", total_recv_bytes/1000000.0, "MB(",last_recv_bytes, "B)");
+    last_recv_bytes =0;
+},1000 );
 
 server.bind(22222, '0.0.0.0');
 
