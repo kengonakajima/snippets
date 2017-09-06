62c36419fa0d2513656b1d60d2ab69e037b063ed
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-09-06 10:25:54 +0900

    usage

--- node/echo/cl.js
+++ node/echo/cl.js
@@ -5,12 +5,21 @@
 var interval_ms = 200;
 var bytes_per_write = 20 * 1024;
 var mbps = (bytes_per_write * (1000.0/interval_ms) * 8 ) / 1000000;
+var host = process.argv[2]
+
+if(!host) {
+    console.log("need host address. Ex: node cl.js 210.140.172.17");
+    process.exit(1);
+}
+    
+
 
 console.log("start sending.. expect Mbps:", mbps );
 
 //
 
 var recv_total_bytes=0;
+var recv_last_bytes=0;
 
 var net = require("net")
 var cl = new net.Socket();
@@ -26,7 +35,8 @@ cl.connect( 22222, "210.140.72.117", function() {
     }, interval_ms );
 
     setInterval( function() {
-        console.log( new Date, " ", recv_total_bytes/1000000.0, "Mbytes received");
+        console.log( new Date, " ", recv_total_bytes/1000000.0, "Mbytes total,", recv_last_bytes, "bytes" );
+        recv_last_bytes=0;
     }, 1000 );
 });
 
@@ -34,6 +44,7 @@ cl.connect( 22222, "210.140.72.117", function() {
 
 cl.on("data", function(data) {
     recv_total_bytes += data.length;
+    recv_last_bytes += data.length;
 });
 
 cl.on("close", function() {
