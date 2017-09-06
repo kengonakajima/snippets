bdb7aaa0859985b9e5897589e647cb30a7422efb
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-09-06 10:15:36 +0900

    bw

--- node/echo/cl.js
+++ node/echo/cl.js
@@ -1,17 +1,42 @@
 // 1Mbpsぐらい送る
+
+
+// 設定
+var interval_ms = 200;
+var bytes_per_write = 20 * 1024;
+var mbps = (bytes_per_write * (1000.0/interval_ms) * 8 ) / 1000000;
+
+console.log("start sending.. expect Mbps:", mbps );
+
 //
+
+var recv_total_bytes=0;
 
 var net = require("net")
 var cl = new net.Socket();
 cl.connect( 22222, "210.140.72.117", function() {
     console.log("connected");
-    cl.write("hoge");   
+
+    var s = "";
+    var letters = "abcdefghijkljmnopqrstuvwxyz0123456789";
+    for(var i=0; i<bytes_per_write; i++) s+= letters[Math.floor(Math.random()*letters.length)];
+
+    setInterval( function() {
+        cl.write(s);
+    }, interval_ms );
+
+    setInterval( function() {
+        console.log( new Date, " ", recv_total_bytes/1000000.0, "Mbytes received");
+    }, 1000 );
 });
 
+
+
 cl.on("data", function(data) {
-    console.log("data:",data);
+    recv_total_bytes += data.length;
 });
 
 cl.on("close", function() {
     console.log("closed");
 });
+
