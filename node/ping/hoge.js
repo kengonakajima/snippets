930ed5b3a261c88ab78f44bda91135ed7b99b7af
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-09-15 11:27:38 +0900

    500ms

--- node/ping/hoge.js
+++ node/ping/hoge.js
@@ -1,13 +1,16 @@
 var ping = require("net-ping")
 
-var session = ping.createSession();
+
 
 var target = "8.8.8.8";
 
-session.pingHost( target, function(error,target) {
-    if(error) {
-        console.log("target:" + target + " : " + error.toString() );
-    } else {
-        console.log("target:" + target + " is alive");
-    }
-});
+setInterval( function() {
+    var session = ping.createSession();
+    session.pingHost( target, function(error,target) {
+        if(error) {
+            console.log("target:" + target + " : " + error.toString() );
+        } else {
+            console.log("target:" + target + " is alive");
+        }
+    });    
+}, 500 );
