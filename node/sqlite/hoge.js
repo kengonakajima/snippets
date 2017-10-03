bc7b109784624be3bcf4ab8e5e1910f66e59fc26
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-10-04 06:31:01 +0900

    sqlite3

---
+++ node/sqlite/hoge.js
@@ -0,0 +1,24 @@
+var sqlite3 = require("sqlite3");
+
+sqlite3.verbose();
+
+var db = new sqlite3.Database(':memory:');
+db.serialize();
+db.on('error', function (err) {
+    console.error(err);
+    process.exit(1);
+});
+
+db.run('CREATE TABLE IF NOT EXISTS sample (info TEXT)');
+for (var i = 0; i < 10; i++) {
+    db.run('INSERT INTO sample VALUES (?)', 'i = ' + i);
+}
+
+db.each('SELECT rowid AS id, info FROM sample', function (err, row) {
+    console.log(row.id + ': ' + row.info);
+});
+
+
+db.close();
+
+
