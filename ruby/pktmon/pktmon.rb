3670a164a4efd60284e9992eb0df5ea358c7a799
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-10-25 06:53:44 +0900

    pktmo

--- ruby/pktmon/pktmon.rb
+++ ruby/pktmon/pktmon.rb
@@ -5,27 +5,37 @@ if !dev then
   exit 1
 end
 
-$last_sent=0
-$last_recv=0
-$sent=0
-$recv=0
+$last_sent_b=0
+$last_recv_b=0
+$last_sent_p=0
+$last_recv_p=0
+$sent_b=0
+$recv_b=0
+$sent_p=0
+$recv_p=0
 
 while true
   out=`ifconfig #{dev}`
   out.split("\n").each do |line|
     tks=line.split(" ")
     if(tks[0]=="RX" and tks[1]=="packets") then
-      n = tks[2].to_i
-      $recv = n - $last_recv
-      $last_recv=n
+      pn = tks[2].to_i
+      bn = tks[4].to_i
+      $recv_p = pn - $last_recv_p
+      $recv_b = bn - $last_recv_b
+      $last_recv_p=pn
+      $last_recv_b=bn
     end
     if(tks[0]=="TX" and tks[1]=="packets") then
-      n = tks[2].to_i
-      $sent = n - $last_sent
-      $last_sent=n
+      pn = tks[2].to_i
+      bn = tks[4].to_i
+      $sent_p = pn - $last_sent_p
+      $sent_b = bn - $last_sent_b
+      $last_sent_p=pn
+      $last_sent_b=bn
     end
   end
   ts = Time.now.to_i
-  print "[#{ts}] Recv:#{$recv} Sent:#{$sent}\n"
+  print "[#{ts}] pkt r:#{$recv_p} s:#{$sent_p}  bytes r:#{$recv_b/1000000.0}B s:#{$recv_b/1000000.0}M\n"
   sleep 1
 end
