620428986469dbf5d10ba9e7fb4c6aad3032ccf5
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-10-25 13:54:43 +0900

    er

--- ruby/pktmon/netstat.rb
+++ ruby/pktmon/netstat.rb
@@ -3,6 +3,8 @@ $total_p=0
 $total_p_d=0
 $total_delivered_p=0
 $total_delivered_p_d=0
+$total_e=0
+$total_e_d=0
 
 while true
   nsout=`netstat -s`
@@ -20,10 +22,16 @@ while true
       $total_delivered_p=p
       $total_delivered_p_d=d
     end
+    if line =~ /packet receive error/ then
+      p=line.split(/\s+/)[0].to_i
+      d = p - $total_e
+      $total_e=p
+      $total_e_d=d      
+    end
   end
   ts = Time.now
   discarded=$total_p_d - $total_delivered_p_d
-  print "[#{ts}] Packets in:#{$total_p_d} delivered:#{$total_delivered_p_d} discarded:#{discarded}\n"
+  print "[#{ts}] Packets in:#{$total_p_d} delivered:#{$total_delivered_p_d} discarded:#{discarded} err:#{$total_e_d}\n"
   sleep 1
 
 end
