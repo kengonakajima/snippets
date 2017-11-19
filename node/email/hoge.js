3f54f5319af1e1dfb4309fc82c0a824b18fb07ec
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-11-20 06:19:04 +0900

    emailval

---
+++ node/email/hoge.js
@@ -0,0 +1,5 @@
+var validator = require("email-validator");
+console.log( validator.validate("test@gmail.com") );
+console.log( validator.validate("te..st@gmail.com") ); // false
+console.log( validator.validate("tes<t@gmail.com") ); // false
+console.log( validator.validate("<test@gmail.com>") ); // false
