eec039b0879f55086e98d55001a5daafb0b302dc
kengo nakajima <kengo.nakajima@gmail.com>
2017-12-14 11:36:23 +0900

    json

--- php/slim/hoge.php
+++ php/slim/hoge.php
@@ -6,7 +6,8 @@ use \Slim\App;
 $app = new \Slim\App();
 
 $app->get("/", function() {
-  echo "app root!";
+  $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+  echo json_encode($arr);
 });
 
 $app->run();
