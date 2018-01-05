4dca948d76bf9974bc2cec43f751b38a74a55e5d
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-05 10:48:54 +0900

    url

---
+++ node/urlencode/hoge.js
@@ -0,0 +1,7 @@
+a="iVBORw0KGgoAAAANSUhEUgAABXgAAASwCAYAAABBxyJaAAAgAElEQVR4XuydM"
+b=new Buffer(a,"base64");
+s=b.toString();
+console.log(b);
+console.log(s);
+console.log(s[0]);
+console.log(s[1]);
