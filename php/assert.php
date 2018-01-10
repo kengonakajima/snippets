7217ced0967d67dca8ad7a35b91f55d37173d57e
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-10 13:56:45 +0900

    ex

---
+++ php/assert.php
@@ -0,0 +1,8 @@
+<?php
+
+assert(1);
+echo "aaaa";
+
+assert(0);
+
+echo "bbb";
---
+++ php/throw.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+    echo "zzz";
+    eccc();
+    echo "yyy";    
+} catch(Exception $e) {
+    echo "eeee:",$e;
+}
