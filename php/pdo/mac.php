947c6bf25e8d2c48e03099c51d64ef35cfd09dfb
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-12 15:46:52 +0900

    ln-

--- php/pdo/mac.php
+++ php/pdo/mac.php
@@ -1,6 +1,7 @@
 <?php
 $user="root";
 $pass="";
+// ln -s /tmp/mysql.sock /var/mysql/mysql.sock
 $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
 foreach($dbh->query("select * from hoge") as $row) {
   print_r($row);
