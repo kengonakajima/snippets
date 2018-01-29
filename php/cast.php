ea077254869202b34d8353feeae37743e4a8379d
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-29 14:54:32 +0900

    cast

--- php/cast.php
+++ php/cast.php
@@ -4,5 +4,14 @@ $a = "aho";
 
 $i = (int)$a;
 
-echo $i;
-echo $a;
+echo $i, "\n";
+echo $a, "\n";
+
+$b =(bool)$a;
+echo $b, "\n";
+
+echo (bool)"false", "\n";
+echo (bool)"true", "\n";
+echo (bool)"F", "\n";
+echo (bool)0, "\n";
+echo (bool)0.1, "\n";
