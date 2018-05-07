b30e6971475f8d3f502f565c650c65638afb15e4
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-05-07 17:07:20 +0900

    no

---
+++ php/pdo/no.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'idiorm.php';
+
+ORM::configure('mysql:host=localhost;dbname=test');
+ORM::configure('username', 'root');
+ORM::configure('password', 'monobit');
+date_default_timezone_set( "Asia/Tokyo");
+
+echo "start\n";
+
+$val=[ "id"=>191 ];
+
+$a=ORM::forTable("aho")->where("id",191)->find_many();
+
+var_dump( (int)$a[0]->id);
+
+
+
+$a=ORM::forTable("aho")->where("id",9191)->find_many();
+
+var_dump( $a[0]->id);
+
+var_dump(NULL);
+
+var_dump(NULL==NULL);
+
+$a=NULL;
+
+var_dump($a==NULL);
