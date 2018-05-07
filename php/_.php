1f058cabfb187c0cb802cf9979bab1fe0e87f01f
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-05-07 16:59:45 +0900

    cas

---
+++ php/_.php
@@ -0,0 +1,7 @@
+<?php
+
+    function abc() { echo "abc\n"; }
+function a_b_c() { echo "cccccccccccccccccccccc\n"; }
+
+
+a_bc();
---
+++ php/case.php
@@ -0,0 +1,21 @@
+<?php
+
+function getA($a) {
+    echo $a, "\n";
+}
+
+//error
+//function geta($a) {
+//    echo $a, "aaa\n";
+//}
+
+
+getA("aho");
+geta("hoge");
+
+
+$a="ahoaho";
+$A="hogehoge";
+
+echo $a,"\n";
+echo $A,"\n";
