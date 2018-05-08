fd23eb421b3b30cc5087155c377337334701e590
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-05-08 13:14:26 +0900

    log

---
+++ php/pdo/q.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'idiorm.php';
+
+ORM::configure('mysql:host=localhost;dbname=test');
+ORM::configure('username', 'root');
+ORM::configure('password', 'monobit');
+date_default_timezone_set( "Asia/Tokyo");
+
+ORM::configure("logging",true);
+    
+echo "start\n";
+
+$val=[ "id"=>191 ];
+
+$a=ORM::forTable("zako")->find_many();
+
+var_dump($a);
+
+echo ORM::getLastQuery();
+
+
