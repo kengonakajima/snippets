355bcbe680cbd1a7d5bb303690ccb242c6c0fe5d
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-08-29 05:11:13 +0900

    cost

--- node/cell/hoge.js
+++ node/cell/hoge.js
@@ -2,6 +2,62 @@
 
 var fsz=256;
 var cell_num=fsz*fsz*fsz;
+
+class Cell {
+    constructor() {
+        this.bt=0;
+        this.lt=0;
+        this.qt=0;
+        this.aaaaa=0;
+        this.bbbb_bb=0;
+    }
+}
+g_cells=new Array(cell_num);
+function set_bt_o(x,y,z,bt) {
+    var ind=(x+z*fsz+y*fsz*fsz);
+    g_cells[ind].bt=bt;
+}
+function get_bt_o(x,y,z) {
+    var ind=(x+z*fsz+y*fsz*fsz);
+    return g_cells[ind].bt;
+}
+
+st=now();
+for(var i=0;i<g_cells.length;i++) {
+    g_cells[i]=new Cell();
+}
+et=now();
+console.log("new_o:",et-st);
+
+
+for(var i=0;i<10;i++) {    
+    var st=now();
+    for(var y=0;y<fsz;y++){
+        for(var z=0;z<fsz;z++) {
+            for(var x=0;x<fsz;x++) {
+                set_bt_o(x,y,z,1);
+            }
+        }
+    }
+    var et=now();
+    console.log("set_o:",et-st);
+    st=now();
+    for(var y=0;y<fsz;y++){
+        for(var z=0;z<fsz;z++) {
+            for(var x=0;x<fsz;x++) {
+                get_bt_o(x,y,z);
+            }
+        }
+    }
+    et=now();
+    console.log("get_bt_o:",et-st);
+    
+}
+
+
+
+//////////////
+
 var b_per_cell=16;
 g_ary=new Uint8Array(cell_num*b_per_cell);
 
