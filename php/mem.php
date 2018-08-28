1aa86045e33fc266b62018b1cb2a92e17db6ad02
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-08-28 15:41:59 +0900

    mem

--- php/mem.php
+++ php/mem.php
@@ -3,17 +3,29 @@
 $mc=new Memcached();
 
 $mc->addServer("localhost",11211);
+$mc->set("hogekey", "helloval",100 );
 
-$data="hello";
-$t0=gettimeofday(true);
-$mc->set("hoge", $data, 100 );
-$t1=gettimeofday(true);
-$get_data = $mc->get("hoge");
-$t2=gettimeofday(true);
+$get_data = $mc->get("hogekey");
 
-echo $get_data, "\n";
+echo "get data:", $get_data, "\n";
 
-echo ($t2-$t1), "\n";
-echo ($t1-$t0), "\n";
+$mc->set("app.localfugakey", "fugaval", 1000 );
+$mc->set("app.localpiyokey", "piyoval", 1000 );
+
+sleep(2);
+$keys=$mc->getAllKeys();
+
+var_dump($keys);
+
+$env = "app.local";
+$keys = preg_grep("/^{$env}/",$keys);
+$ret=array();
+if(!empty($keys)) {
+    foreach($keys as $k) {
+        $ret[]=substr($k,strlen($env));
+    }
+}
+var_dump($ret);
+
 
     
