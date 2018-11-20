f940a70cc516a622aa6df2f88614d0207e9aa81e
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-11-20 11:50:18 +0900

    api

---
+++ php/iwate/api.php
@@ -0,0 +1,26 @@
+<?php
+$pdo=new PDO('mysql:host=127.0.0.1;dbname=test',"root","monobit");
+
+$user_id=$_REQUEST['user_id'];
+$item_id=$_REQUEST['item_id'];
+
+$res=$pdo->query("select * from items where id=" . $item_id );
+$row=$res->fetch();
+
+//print_r($row);
+$price=$row['price'];
+//print_r($price);
+
+$res=$pdo->query("update users set wallet=wallet-" . $price . ", item0=" . $item_id . " where id=" . $user_id );
+
+print_r($res);
+
+$res=$pdo->query("select wallet from users where id=" . $user_id );
+$row=$res->fetch();
+$wallet=$row["wallet"];
+
+$a=array( 'result' => "success", "price" => $price, "wallet" => $wallet);
+
+echo json_encode($a);
+
+//foreach($res as $row) {    print_r($row);}
