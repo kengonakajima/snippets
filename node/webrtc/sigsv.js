5bf8c47670719885633154f9f1a30110d0a1c126
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-05-24 14:18:34 +0900

    sigsv

---
+++ node/webrtc/sigsv.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let WebSocketServer = require('ws').Server;
+let port = 9000; 
+let wsServer = new WebSocketServer({ port: port });
+console.log('websocket server start. port=' + port);
+
+wsServer.on('connection', function(ws) {
+  console.log('-- websocket connected --');
+  ws.on('message', function(message) {
+    wsServer.clients.forEach(function each(client) {
+      if (isSame(ws, client)) {
+        console.log('- skip sender -');
+      }
+      else {
+        client.send(message);
+      }
+    });
+  });
+});
+
+function isSame(ws1, ws2) {
+  // -- compare object --
+  return (ws1 === ws2);
+}
