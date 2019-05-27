75b4fe94ef15f4f00b5eb8bc7ce3895eba9117c4
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-05-27 14:26:58 +0900

    rot

--- cpp/gl3/main.cpp
+++ cpp/gl3/main.cpp
@@ -385,18 +385,6 @@ int main() {
     // Or, for an ortho camera :
     //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
 
-    // Camera matrix
-    glm::mat4 view = glm::lookAt(
-                                 glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
-                                 glm::vec3(0,0,0), // and looks at the origin
-                                 glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-                                 );
-
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 model = glm::mat4(1.0f);
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 mvp = projection * view * model; // Remember, matrix multiplication is the other way around
-
 
     // Get a handle for our "MVP" uniform
     // Only during the initialisation
@@ -424,6 +412,21 @@ int main() {
 
         // Send our transformation to the currently bound shader, in the "MVP" uniform
         // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        // Camera matrix
+        static float r=0;
+        r+=0.1;
+        glm::mat4 view = glm::lookAt(
+                                     glm::vec3(::sin(r)*4,3,3), // Camera is at (4,3,3), in World Space
+                                     glm::vec3(0,0,0), // and looks at the origin
+                                     glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                                     );
+
+        // Model matrix : an identity matrix (model will be at the origin)
+        glm::mat4 model = glm::mat4(1.0f);
+        // Our ModelViewProjection : multiplication of our 3 matrices
+        glm::mat4 mvp = projection * view * model; // Remember, matrix multiplication is the other way around
+
+        
         glUniformMatrix4fv(matrix_id, 1, GL_FALSE, &mvp[0][0]);
 
 		// Bind our texture in Texture Unit 0
