4670b52e51e991ba26ce8a77bce3b8fac71bc349
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-05-27 11:26:43 +0900

    color_cube

--- cpp/gl3/main.cpp
+++ cpp/gl3/main.cpp
@@ -39,18 +39,24 @@ const char vsh_simple_glsl[] =
     //    "# version 330 core\n"
     "#version 330 core\n"
     "layout(location = 0) in vec3 vertexPosition_modelspace;\n"
+    // Notice that the "1" here equals the "1" in glVertexAttribPointer
+    "layout(location = 1) in vec3 vertexColor;\n"
     "uniform mat4 MVP;\n"
+    "out vec3 fragmentColor;\n"        
     "void main() {\n"
     //    " gl_Position.xyz=vertexPosition_modelspace;\n"
     //    " gl_Position.w=1.0;\n"
     " gl_Position=MVP*vec4(vertexPosition_modelspace,1);\n"
+    "  fragmentColor = vertexColor;\n"        
     "}\n";
 
 const char fsh_red_glsl[] = 
     "# version 330 core\n"
-    "out vec3 color;\n"    
+    "in vec3 fragmentColor;\n"
+    "out vec3 color;\n"
     "void main(){\n"
-    "  color = vec3(1,0,0);\n"
+    //    "  color = vec3(1,0,0);\n"
+    "  color = fragmentColor;\n"    
     "}\n";
 
 void errorCallback(int code, const char* err_str) {
@@ -163,10 +169,90 @@ int main() {
     glGenVertexArrays(1,&vert_ary_id);
     glBindVertexArray(vert_ary_id);
 
+#if 0    
     static const GLfloat g_vertex_buffer_data[] = {
                                                    -1.0f, -1.0f, 0.0f,
                                                    1.0f, -1.0f, 0.0f,
                                                    0.0f,  1.0f, 0.0f,
+    };
+#endif
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+                                                   -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+                                                   -1.0f,-1.0f, 1.0f,
+                                                   -1.0f, 1.0f, 1.0f, // triangle 1 : end
+                                                   1.0f, 1.0f,-1.0f, // triangle 2 : begin
+                                                   -1.0f,-1.0f,-1.0f,
+                                                   -1.0f, 1.0f,-1.0f, // triangle 2 : end
+                                                   1.0f,-1.0f, 1.0f,
+                                                   -1.0f,-1.0f,-1.0f,
+                                                   1.0f,-1.0f,-1.0f,
+                                                   1.0f, 1.0f,-1.0f,
+                                                   1.0f,-1.0f,-1.0f,
+                                                   -1.0f,-1.0f,-1.0f,
+                                                   -1.0f,-1.0f,-1.0f,
+                                                   -1.0f, 1.0f, 1.0f,
+                                                   -1.0f, 1.0f,-1.0f,
+                                                   1.0f,-1.0f, 1.0f,
+                                                   -1.0f,-1.0f, 1.0f,
+                                                   -1.0f,-1.0f,-1.0f,
+                                                   -1.0f, 1.0f, 1.0f,
+                                                   -1.0f,-1.0f, 1.0f,
+                                                   1.0f,-1.0f, 1.0f,
+                                                   1.0f, 1.0f, 1.0f,
+                                                   1.0f,-1.0f,-1.0f,
+                                                   1.0f, 1.0f,-1.0f,
+                                                   1.0f,-1.0f,-1.0f,
+                                                   1.0f, 1.0f, 1.0f,
+                                                   1.0f,-1.0f, 1.0f,
+                                                   1.0f, 1.0f, 1.0f,
+                                                   1.0f, 1.0f,-1.0f,
+                                                   -1.0f, 1.0f,-1.0f,
+                                                   1.0f, 1.0f, 1.0f,
+                                                   -1.0f, 1.0f,-1.0f,
+                                                   -1.0f, 1.0f, 1.0f,
+                                                   1.0f, 1.0f, 1.0f,
+                                                   -1.0f, 1.0f, 1.0f,
+                                                   1.0f,-1.0f, 1.0f
+    };
+    
+    static const GLfloat g_color_buffer_data[] = {
+                                                  0.583f,  0.771f,  0.014f,
+                                                  0.609f,  0.115f,  0.436f,
+                                                  0.327f,  0.483f,  0.844f,
+                                                  0.822f,  0.569f,  0.201f,
+                                                  0.435f,  0.602f,  0.223f,
+                                                  0.310f,  0.747f,  0.185f,
+                                                  0.597f,  0.770f,  0.761f,
+                                                  0.559f,  0.436f,  0.730f,
+                                                  0.359f,  0.583f,  0.152f,
+                                                  0.483f,  0.596f,  0.789f,
+                                                  0.559f,  0.861f,  0.639f,
+                                                  0.195f,  0.548f,  0.859f,
+                                                  0.014f,  0.184f,  0.576f,
+                                                  0.771f,  0.328f,  0.970f,
+                                                  0.406f,  0.615f,  0.116f,
+                                                  0.676f,  0.977f,  0.133f,
+                                                  0.971f,  0.572f,  0.833f,
+                                                  0.140f,  0.616f,  0.489f,
+                                                  0.997f,  0.513f,  0.064f,
+                                                  0.945f,  0.719f,  0.592f,
+                                                  0.543f,  0.021f,  0.978f,
+                                                  0.279f,  0.317f,  0.505f,
+                                                  0.167f,  0.620f,  0.077f,
+                                                  0.347f,  0.857f,  0.137f,
+                                                  0.055f,  0.953f,  0.042f,
+                                                  0.714f,  0.505f,  0.345f,
+                                                  0.783f,  0.290f,  0.734f,
+                                                  0.722f,  0.645f,  0.174f,
+                                                  0.302f,  0.455f,  0.848f,
+                                                  0.225f,  0.587f,  0.040f,
+                                                  0.517f,  0.713f,  0.338f,
+                                                  0.053f,  0.959f,  0.120f,
+                                                  0.393f,  0.621f,  0.362f,
+                                                  0.673f,  0.211f,  0.457f,
+                                                  0.820f,  0.883f,  0.371f,
+                                                  0.982f,  0.099f,  0.879f
     };
 
     // This will identify our vertex buffer
@@ -177,6 +263,11 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER,colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(g_color_buffer_data),g_color_buffer_data,GL_STATIC_DRAW);
     
 
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
@@ -203,7 +294,12 @@ int main() {
 
 
     //////////
-    
+
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
     print("start");
     while(1) {
         if(glfwWindowShouldClose(window) )break;
@@ -227,8 +323,19 @@ int main() {
                               0,                  // stride
                               (void*)0            // array buffer offset
                               );
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+                              1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+                              3,                                // size
+                              GL_FLOAT,                         // type
+                              GL_FALSE,                         // normalized?
+                              0,                                // stride
+                              (void*)0                          // array buffer offset
+                              );
+        
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 12*3); // Starting from vertex 0; 3 vertices total -> 1 triangle
         glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
