bc91d4eccbf4395db59d6763edac553fc296fd74
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-08-29 12:37:26 +0900

    hoge

--- c/h/hoge.c
+++ c/h/hoge.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
-
-    unsigned int hash(unsigned int x) {
-        x = ((x >> 16) ^ x) * 0x119de1f3;
-        x = ((x >> 16) ^ x) * 0x119de1f3;
-        x = (x >> 16) ^ x;
-        return x;
-    }
-int n[1024];
-
+unsigned int hash(unsigned int x) {
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = (x >> 16) ^ x;
+    return x;
+}
+unsigned int hash2(unsigned int i) {
+    unsigned long long il=i;
+    il *=2654435761;
+    il = il % 4294967296;
+    return (unsigned int)il;
+}
+#define N 5000
+int n[N];
 int main() {
-    for(int i=0;i<100000000;i++) {
-        unsigned int h=hash(i);
-        n[h%1024]++;
+    for(unsigned int i=0;i<1000000;i++) {
+        unsigned int h=hash2( random());
+        n[h%N]++;
     }
-    for(int i=0;i<1024;i++) {
+    for(int i=0;i<N;i++) {
         printf("%d %d\n", i, n[i]);
     }
 }
