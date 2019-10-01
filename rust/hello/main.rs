ba29a02669a9024f53e20407c06bb3834891ab37
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-10-01 14:38:08 +0900

    optim

--- rust/hello/main.rs
+++ rust/hello/main.rs
@@ -4,7 +4,8 @@ use std::io::prelude::*;
 fn main() -> std::io::Result<()> {
   let mut file = File::create("hoge_rs.txt")?;
   for i in 0..1000000 {
-    write!(&mut file, "hoge:{}\n",i)?;  
+    let s = format!("hoge:{}\n",i);  
+    file.write( & s.into_bytes() )?;
   }
   Ok(())
 }
