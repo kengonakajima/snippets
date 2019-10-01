8de8813bb6449e981883c88c119c39c8e91bc00b
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-10-01 14:58:57 +0900

    ign

--- cs/cs_tmp/f.c
+++ cs/cs_tmp/f.c
@@ -3,7 +3,7 @@
 int main() {
     FILE *fp=fopen("hoge_c.txt","w");
     for(int i=0;i<1000000;i++) {
-        fprintf(fp,"hoge:%d",i);
+        fprintfntf(fp,"hoge:%d",i);
     }
     fclose(fp);
 }
---
+++ golang/f.go
@@ -0,0 +1,24 @@
+package main
+import(
+	"fmt"
+//	"bufio"
+//	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+	f,err :=os.Create("hoge_go.txt")
+	check(err)
+
+//	d2 := []byte{115, 111, 109, 101, 10}
+	for i := 0; i < 1000000 ; i++ {
+		s := fmt.Sprintf("hoge:%d\n",i)
+		f.Write([]byte(s))		
+	}
+}
