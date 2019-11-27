a03ff77627262c25f9e49315899545540c7b073f
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-11-27 14:58:47 +0900

    scriptnode

--- js/webaudio/main.js
+++ js/webaudio/main.js
@@ -32,7 +32,7 @@ var g_last_played_source = null;
 // サウンドを再生
 var playSound = function(buffer) {
     console.log(buffer);
-    // source を作成
+
     var source = context.createBufferSource();
     // buffer をセット
     source.buffer = buffer;
@@ -40,23 +40,56 @@ var playSound = function(buffer) {
     source.connect(context.destination);
     // 再生
     source.start(0);
+
     g_last_played_source = source;
 };
 
-function makeMemoryAudioBuf() {
-    var ch=2;
-    var frameCount = context.sampleRate*2.0;
-    var outbuf = context.createBuffer( ch, frameCount, context.sampleRate );
+var t=0;
 
-    for (var chi = 0; chi < ch; chi++) {
-        var b = outbuf.getChannelData(chi);
-        for (var i = 0; i < frameCount; i++) {
-            b[i] = Math.sin(i/10.0);
-        }
-    }    
+function genSamples(outbuf,frameCount,hz) {
+    var t_incr = 2 * Math.PI * hz / 48000.0;
+    var b0 = outbuf.getChannelData(0);
+    var b1 = outbuf.getChannelData(1);
+    for (var i = 0; i < frameCount; i++) {    
+        b0[i] = Math.sin(t);
+        b1[i] = b0[i]
+        t+=t_incr
+    }
+}
+
+function makeMemoryAudioBuf(frameCount,hz) {
+    var ch=2;
+    var outbuf = context.createBuffer( ch, frameCount, context.sampleRate );
+    genSamples(outbuf,frameCount,hz);
     return outbuf;
 }
-var membuf = makeMemoryAudioBuf();
+var membuf = makeMemoryAudioBuf(context.sampleRate*2,440);
+
+//var mediaSource = new MediaSource();
+var g_tone=1;
+
+var sn = context.createScriptProcessor(256,1,1);
+console.log("sn:",sn);
+sn.onaudioprocess = function(audioProcessingEvent) {
+    var t_incr = 2 * Math.PI * 440.0*3 / 48000.0;
+    var inputBuffer = audioProcessingEvent.inputBuffer;
+    var outputBuffer = audioProcessingEvent.outputBuffer;
+    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+        var inputData = inputBuffer.getChannelData(channel);
+        var outputData = outputBuffer.getChannelData(channel);
+        for (var sample = 0; sample < inputBuffer.length; sample++) {
+            outputData[sample] = inputData[sample];
+            outputData[sample] += (Math.sin(t)*0.2);
+            t+=t_incr/2.0;
+        }
+    }
+}
+var dummysrc = context.createConstantSource();
+dummysrc.connect(sn);
+sn.connect(context.destination);
+
+
+
 
 // main
 window.onload = function() {  
@@ -79,5 +112,11 @@ window.onload = function() {
                 g_last_played_source.stop(0);
             }
         }
+        var btnstream = document.getElementById("stream");
+        btnstream.onclick = function() {
+            dummysrc.start(0)   ;
+        }
     });
+
+    
 };
