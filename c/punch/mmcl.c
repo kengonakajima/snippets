225107a438f8286213a94dd65cd29184e062edc2
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-01-26 08:19:16 +0900

    mmcl

---
+++ c/punch/mmcl.c
@@ -0,0 +1,38 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h> 
+#include <fcntl.h>
+
+ 
+int main(int argc, char* argv[]) {
+    if(argc!=2) {
+        printf("need server ip\n");
+        return 1;
+    }
+
+    const int N=300;
+    int sp=10000;
+    int fds[N];    
+    for(int i=0;i<N;i++) {
+        fds[i]=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        assert(fds[i]>0);
+        
+        struct sockaddr_in saddr;
+        int slen=sizeof(saddr);
+        memset((char *) &saddr, 0, sizeof(saddr));
+        saddr.sin_family = AF_INET;
+        saddr.sin_port = htons(sp+i);
+        int r=inet_aton(argv[1], &saddr.sin_addr);
+        assert(r==1);
+        
+        r=sendto(fds[i], "hi", 2, 0, (struct sockaddr*)(&saddr), slen);
+        assert(r>=0);
+    }
+    return 0;
+}
