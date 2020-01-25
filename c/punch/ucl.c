774a6b2fc2cc2f6ab7a14870360d9c03818cd9b4
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-01-26 08:38:08 +0900

    port

--- c/punch/ucl.c
+++ c/punch/ucl.c
@@ -6,16 +6,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h> 
+#include <assert.h>
+#include <fcntl.h>
+
+int32_t set_socket_nonblock(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    assert(flags >= 0);
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+
 
 int main(int argc, char* argv[]) {
-    if(argc!=2) {
-        printf("need server ip\n");
+    if(argc!=3) {
+        printf("need server ip port\n");
         return 1;
     }
     struct sockaddr_in si_me, si_sv;
     int s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     assert(s>0);
+    set_socket_nonblock(s);
 
     memset((char *) &si_me, 0, sizeof(si_me));
     si_me.sin_family = AF_INET;
@@ -24,20 +34,28 @@ int main(int argc, char* argv[]) {
  
     memset((char *) &si_sv, 0, sizeof(si_sv));
     si_sv.sin_family = AF_INET;
-    si_sv.sin_port = htons(10000);
+    si_sv.sin_port = htons(atoi(argv[2]));
     int r=inet_aton(argv[1], &si_sv.sin_addr);
     assert(r==1);
 
-    socklen_t slen=sizeof(si_sv);
-    r=sendto(s, "hi", 2, 0, (struct sockaddr*)(&si_sv), slen);
-    assert(r>=0);
+    int cnt=0;
+    while(1) {
+        cnt++;
 
-    char buf[100];
-    struct sockaddr_in si_other;
-    slen=sizeof(si_other);
-    r=recvfrom(s, &buf, sizeof(buf), 0, (struct sockaddr*)(&si_other), &slen);
-    assert(r>=0);
-    printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        
+        if((cnt%1000)==0) {
+            socklen_t slen=sizeof(si_sv);
+            r=sendto(s, "hi", 2, 0, (struct sockaddr*)(&si_sv), slen);
+            assert(r>=0);
+        }
+
+        char buf[100];
+        struct sockaddr_in si_other;
+        socklen_t slen=sizeof(si_other);
+        r=recvfrom(s, &buf, sizeof(buf), 0, (struct sockaddr*)(&si_other), &slen);
+        if(r>0) {
+            printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        }
+        usleep(1000);
+    }        
     return 0;
 }
