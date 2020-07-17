c9fc1ec9513b0685c80ef5769f768308d809521d
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-07-18 07:06:00 +0900

    chunky

---
+++ ruby/chunky/hoge.rb
@@ -0,0 +1,7 @@
+require "chunky_png"
+
+png = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
+png[1,1] = ChunkyPNG::Color.rgba(10, 20, 30, 128)
+png[2,1] = ChunkyPNG::Color('black @ 0.5')
+png.save('filename.png', :interlace => false)
+
