3b68dcbae1b03344fa43175dfd7f4eeba5f1acba
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-09-04 15:28:41 +0900

    istft

--- py/snd/stft.py
+++ py/snd/stft.py
@@ -6,7 +6,9 @@ import scipy.signal as sp
 
 pa.datasets.CMUArcticCorpus(basedir="./CMU_ARCTIC",download=True,speaker=["aew","axb"])
 
-sample_wave_file="./CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"
+#sample_wave_file="./CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"
+
+sample_wave_file="./howling.wav"
 
 wav=wave.open(sample_wave_file)
 
@@ -37,3 +39,17 @@ plt.xlabel("sec")
 plt.ylabel("hz")
 plt.savefig("./spectrogram.png")
 plt.show()
+
+t,data_post=sp.istft(stft_data,fs=wav.getframerate(), window="hann",nperseg=512,noverlap=256)
+data_post=data_post*np.iinfo(np.int16).max
+data_post=data_post.astype(np.int16)
+print(data_post)
+
+
+wave_out=wave.open("post.wav","w")
+wave_out.setnchannels(1)
+wave_out.setsampwidth(2)
+wave_out.setframerate(wav.getframerate())
+wave_out.writeframes(data_post)
+wave_out.close()
+
