0d7a3af9ccdc0824f8045faa36a78591fc666d9a
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-11-30 16:01:34 +0900

    ja

--- aws/polly/gen.bash
+++ aws/polly/gen.bash
@@ -1,3 +1,3 @@
-aws polly synthesize-speech --region ap-northeast-1 --profile kengonakajima --output-format pcm --text "hello" --voice-id "Salli" hogespeech.pcm
+aws polly synthesize-speech --region ap-northeast-1 --profile kengonakajima --output-format pcm --text "helloにほんごどんな漢字" --voice-id "Mizuki" hogespeech.pcm
 
 ffmpeg -f s16le -ar 8k -ac 2 -i hogespeech.pcm out.wav
