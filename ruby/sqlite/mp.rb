7dcefa2472753dc81049335d5334048d2b97614c
kengonakajima <kengo.nakajima@gmail.com>
2021-04-05 00:13:57 +0000

    mp

--- ruby/sqlite/mp.rb
+++ ruby/sqlite/mp.rb
@@ -5,25 +5,26 @@ require "sqlite3"
 
 $db = SQLite3::Database.new("_mp.db")
 
-$db.execute( "create table if not exists entries ( id varchar(80), doc_id varchar(16) )" )
-begin
-  $db.execute( "create index idindex on entries(id)")
-rescue
-end
 
 
 def do_query(q)
   while true
     begin
       $db.execute(q)
-    rescue
-      sleep 0.1
-      STDERR.print "waiting for lock #{Process.pid}\n"
-      next
+    rescue Exception => ex
+      if ex.code == 5 then
+        sleep 0.1
+        STDERR.print "waiting for lock #{Process.pid}\n"
+        next
+      end
+      STDERR.print ex,"\n"
     end
     break
   end
 end
+
+do_query( "create table if not exists entries ( id varchar(80), doc_id varchar(16) )" )
+do_query( "create index idindex on entries(id)")
 
 do_query("pragma journal_mode=memory")
 
@@ -36,7 +37,8 @@ do_query("pragma journal_mode=memory")
     do_query( "insert into entries values ( 'id#{k}', 'val=#{Process.pid}' ) " )
   end
   do_query("end transaction")
-  STDERR.print "tr end\n"  
+  STDERR.print "tr end\n"
+  sleep 2*rand()
 end
 
 
