d5f28e3e977635a36b138be7e0b8a5e93fbe08f3
Kengo Nakajima <kengo.nakajima@gmail.com>
2021-04-17 09:01:30 +0900

    comment

--- node/rec/recsend.js
+++ node/rec/recsend.js
@@ -1,22 +1,22 @@
-const dgram = require('dgram');
-const recorder = require('node-record-lpcm16'); // nodeモジュールを読み込む
+const dgram = require('dgram');  // データグラム通信用のモジュールを読み込む
+const recorder = require('node-record-lpcm16'); 
 
-const socket = dgram.createSocket('udp4');
+const socket = dgram.createSocket('udp4'); // IPv4のUDPソケットを作成
 
 recorder
     .record({
-        sampleRate: 48000, // マイクデバイスのサンプリングレートを指定
-        channels: 1,  // チャンネル数を指定(モノラル)              
-        recordProgram: 'rec', // 録音用のバックエンドプログラム名を指定
+        sampleRate: 48000, 
+        channels: 1, 
+        recordProgram: 'rec', 
     })
     .stream()
-    .on('error', console.error) // エラーが起きたときにログを出力する
-    .on('data', function(data) { // マイクからデータを受信する無名コールバック関数
-        var vol = Math.abs(data.readInt16LE());  // 配列の先頭のサンプリングデータをひとつ読み込み、音量を得る
-        var ntimes = vol / 512; // 音量が0~32767の値で得られるので512で割る(0~63)
-        var bar = "*".repeat(ntimes); // アスタリスク文字を、音量に応じて0~63回繰り返す
+    .on('error', console.error) 
+    .on('data', function(data) { 
+        var vol = Math.abs(data.readInt16LE());  
+        var ntimes = vol / 512;
+        var bar = "*".repeat(ntimes); 
         console.log("mic volume:", bar);
-        socket.send(data, 0, data.length, 23456, "127.0.0.1");
+        socket.send(data, 0, data.length, 23456, "127.0.0.1"); // ソケットを使ってUDPデータグラムを送信
     });
 console.log('Listening, press Ctrl+C to stop.');
 
