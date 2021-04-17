e7d1767691a82ef49deb2c44750fd743d1d4f8a1
Kengo Nakajima <kengo.nakajima@gmail.com>
2021-04-17 13:45:52 +0900

    rename

--- node/speaker/spkrecv.js
+++ node/speaker/spkrecv.js
@@ -4,23 +4,23 @@ const dgram = require("dgram"); // データグラム通信用のモジュール
 
 
 const receiver=new Readable();
-receiver.buf = new Uint8Array(48000*2); // 受信用のバッファを1秒分確保する(48KHzで1サンプルあたり2バイト)
-receiver.buf_used=0; // 受信用のバッファに、有効なデータが何バイト入っているかを保持する変数
+var recvbuf = new Uint8Array(48000*2); // 受信用のバッファを1秒分確保する(48KHzで1サンプルあたり2バイト)
+var recvbuf_used=0; // 受信用のバッファに、有効なデータが何バイト入っているかを保持する変数
 receiver._read = function(n) {
     var sampleNum = n / 2;
-    var sampleInbuffer = this.buf_used / 2;  // 受信用バッファbufに入っているサンプルの数を計算
+    var sampleInbuffer = recvbuf_used / 2;  // 受信用バッファbufに入っているサンプルの数を計算
     var outary = new Uint8Array(n); // 出力用サンプル
     if(sampleInbuffer>sampleNum) { // 受信用バッファにあるサンプルが要求されたサンプル数以上の数ある場合
         var outdv = new DataView(outary.buffer);  // バイト列にLE16でアクセスするためのDataView
-        var indv = new DataView(this.buf.buffer);
+        var indv = new DataView(recvbuf.buffer);
         for(var i=0;i<sampleNum;i++) {
             var sample=indv.getInt16(i*2,true);
             outdv.setInt16(i*2,sample,true); // 入力となる受信用バッファから出力へsampleNum個のサンプルをコピー
         }
-        for(var i=n;i<this.buf_used;i++) { // 受信用バッファをシフト(コピーし終わった分を捨てる)
-            this.buf[i-n]=this.buf[i];
+        for(var i=n;i<recvbuf_used;i++) { // 受信用バッファをシフト(コピーし終わった分を捨てる)
+            recvbuf[i-n]=recvbuf[i];
         }
-        this.buf_used-=n; // 捨てた分のバイト数だけ減算
+        recvbuf_used-=n; // 捨てた分のバイト数だけ減算
     } else {
         for(var i=0;i<n;i++) { // データが足りないときは無音を再生する
             outary[i]=0;
@@ -32,14 +32,14 @@ receiver._read = function(n) {
 const socket = dgram.createSocket("udp4"); // IPv4のUDPを使うソケットを作る
 socket.on("message", (message,remote) => { // UDPのデータグラムを受信した時のイベント
     console.log("recv data:",remote.address,remote.port,message.length);        
-    if(receiver.buf_used+message.length>receiver.buf.length) { // 受信用のバッファがいっぱいのときは受信しない
+    if(recvbuf_used+message.length>recvbuf.length) { // 受信用のバッファがいっぱいのときは受信しない
         console.log("buffer full");
         return;
     }
     for(var i=0;i<message.length;i++) {
-        receiver.buf[receiver.buf_used+i]=message[i]; // 受信用のバッファにデータをコピーする
+        recvbuf[recvbuf_used+i]=message[i]; // 受信用のバッファにデータをコピーする
     }
-    receiver.buf_used+=message.length; // 受信用バッファの使用量を増やす
+    recvbuf_used+=message.length; // 受信用バッファの使用量を増やす
 });
 
 socket.bind(23456,"0.0.0.0"); // UDPポート番号23456、任意アドレスからのデータグラムを受け入れる
