2a6fff68b0a88904f68789c21128cbe3d5234fad
kengo nakajima <kengo.nakajima@gmail.com>
2022-11-05 13:14:01 +0900

    working

--- node/appstore/as.mjs
+++ node/appstore/as.mjs
@@ -8,9 +8,28 @@ var apiKey=o.apiKey;
 var issuerId=o.issuerId;
 var privateKey=fs.readFileSync("./privkey").toString();
 console.log("ppp:",privateKey,issuerId,apiKey);
-const { read, readAll, create, update, remove } = await api({issuerId, apiKey, privateKey});
+const { read, readAll, create, update, remove, fetch } = await api({issuerId, apiKey, privateKey});
 
 // log all apps
-const { data: apps } = await readAll('https://api.appstoreconnect.apple.com/v1/apps');1
-console.log(apps);
+//const { data: apps } = await readAll('https://api.appstoreconnect.apple.com/v1/apps');
+//console.log(apps);
+
+
+
+// POST https://api.storekit-sandbox.itunes.apple.com/inApps/v1/notifications/test
+const { appStoreVersion } = await fetch('https://api.storekit-sandbox.itunes.apple.com/inApps/v1/notifications/test', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ data: {
+  }})
+})
+      .then(r=> {
+        console.log("r:",r);
+        return r.json();
+      })
+      .then(data=>{
+        console.log("data:",data);
+      });
+
+
 
