c42c3bad83b9da9b0c56f47cc82b0f65fc32f8a2
kengo nakajima <kengo.nakajima@gmail.com>
2023-02-08 12:13:45 +0900

    0

--- node/sse/hoge.js
+++ node/sse/hoge.js
@@ -53,6 +53,6 @@ http.createServer(function (req, res) {
     res.end()
   }
 
-}).listen (port, "127.0.0.1")
+}).listen (port, "0.0.0.0")
 
 console.log("Server running at http://127.0.0.1:" + port + "/")
