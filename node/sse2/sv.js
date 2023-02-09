8ab1c4a0cda2efaa954d96219200f243c5c2d829
kengo nakajima <kengo.nakajima@gmail.com>
2023-02-09 16:26:10 +0900

    post_sse

--- node/sse2/sv.js
+++ node/sse2/sv.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs=require("fs");
 
 function sendServerSendEvent(req, res) {
   res.writeHead(200, {
@@ -7,7 +8,7 @@ function sendServerSendEvent(req, res) {
     'Connection' : 'keep-alive'
   });
 
-  var sseId = (new Date()).toLocaleTimeString();
+  const sseId = (new Date()).toLocaleTimeString();
 
   setInterval(function() {
     writeServerSendEvent(res, sseId, (new Date()).toLocaleTimeString());
@@ -20,9 +21,16 @@ function writeServerSendEvent(res, sseId, data) {
   res.write('id: ' + sseId + '\n');
   res.write("data: new server event " + data + '\n\n');
 }
+function writeSSE(res,data) {
+  const sseId = (new Date()).toLocaleTimeString();
+  res.write('id: ' + sseId + '\n');
+  res.write("data: new server event " + data + '\n\n');  
+}
+
 
 http.createServer(function(req, res) {
-  console.log("req.",req);
+  req.setEncoding("utf-8");
+  console.log("req.",req.method,req.url);
   if (req.headers.accept && req.headers.accept == 'text/event-stream') {
     if (req.url == '/talk') {
       console.log("talk");
@@ -32,12 +40,19 @@ http.createServer(function(req, res) {
       res.writeHead(404);
       res.end();
     }
-  } else {
+  } else if(req.method=="GET"){
     console.log("200");          
     res.writeHead(200, {
       'Content-Type' : 'text/html'
     });
     res.write(fs.readFileSync(__dirname + '/index.html'));
     res.end();
+  } else if(req.method=="POST"){
+    console.log("post!");
+    sendServerSendEvent(req,res);
+    req.on("data", chunk=>{
+      console.log("data:",chunk.toString());
+      writeSSE(res,chunk.toString());
+    });    
   }
 }).listen(9999);
