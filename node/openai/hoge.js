6632882969dbe8060c7bd5f91f4c3589235368eb
kengo nakajima <kengo.nakajima@gmail.com>
2023-02-13 12:21:35 +0900

    stream worked

---
+++ node/openai/hoge.js
@@ -0,0 +1,49 @@
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const wrap = fn => {
+  return (req, res, next) => {
+    return fn(req, res, next).catch(next);
+  }
+};
+
+
+
+
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+wrap( async () => {
+  const res = await openai.createCompletion({
+    model: "text-davinci-003",
+    //    prompt: "犬の名前を三つあげてください。",
+    prompt: "単純なローパスフィルタのコードの重要な部分だけを、C#で書いて下さい",
+    max_tokens: 300,
+    temperature: 0,
+    stream: true
+  },{
+    timeout: 5000    ,
+    responseType: "stream"
+  });
+  //console.log("RES:",res);
+  res.data.on("data",(data)=> {
+    const text=data.toString();
+    if(text.includes("[DONE]")) {
+      console.log("DONE!");
+      process.exit(0);
+    }
+    const json=text.replace(/^data: /,"");
+    //    console.log("json:",json);
+    try {
+      const obj=JSON.parse(json);
+      process.stdout.write(obj.choices[0].text);      
+    } catch(e) {
+      console.log("error:",e,"on json:",json);
+      process.exit(1);
+    }
+  });
+})();
