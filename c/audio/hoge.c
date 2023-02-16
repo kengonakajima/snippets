66d2e50255dca11285a3095f6b4c6f5d3d5c18f8
kengo nakajima <kengo.nakajima@gmail.com>
2023-02-16 18:51:42 +0900

    snd

--- c/audio/hoge.c
+++ c/audio/hoge.c
@@ -4,8 +4,8 @@ int main() {
     short data[n];
     for(int i=0;i<n;i++) {
         data[i]=0;
-        if(i%12000==5000)data[i]=10000;
-        else if(i%12000==5001)data[i]=-10000;
+        if(i%48000==5000)data[i]=10000;
+        else if(i%48000==5001)data[i]=-10000;
     }
     FILE *fp=fopen("click.s16le","w");
     fwrite(data,1,sizeof(data),fp);
