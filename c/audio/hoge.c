9fec3fa0669c54138ed0cf119d75a7c9509bd461
kengo nakajima <kengo.nakajima@gmail.com>
2023-02-16 18:46:02 +0900

    click_audio

---
+++ c/audio/hoge.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    const int n=24000*30;
+    short data[n];
+    for(int i=0;i<n;i++) {
+        data[i]=0;
+        if(i%12000==5000)data[i]=10000;
+        else if(i%12000==5001)data[i]=-10000;
+    }
+    FILE *fp=fopen("click.s16le","w");
+    fwrite(data,1,sizeof(data),fp);
+    fclose(fp);
+}
