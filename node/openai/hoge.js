fc83c3458a3b9ae14d490262e9063606bfb0c93f
kengo nakajima <kengo.nakajima@gmail.com>
2023-03-02 09:14:35 +0900

    credfile

--- node/openai/hoge.js
+++ node/openai/hoge.js
@@ -1,4 +1,4 @@
-
+const fs=require("fs");
 const { Configuration, OpenAIApi } = require("openai");
 
 const wrap = fn => {
@@ -11,17 +11,18 @@ const wrap = fn => {
 
 
 
+const creds=JSON.parse(fs.readFileSync("/Users/ringo/.openai_cred","utf8"));
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: creds.secretKey
 });
 const openai = new OpenAIApi(configuration);
 
 wrap( async () => {
   const res = await openai.createCompletion({
     model: "text-davinci-003",
-    //    prompt: "犬の名前を三つあげてください。",
-    prompt: "単純なローパスフィルタのコードの重要な部分だけを、C#で書いて下さい",
+    prompt: "犬の名前を三つあげてください。",
+    //prompt: "単純なローパスフィルタのコードの重要な部分だけを、C#で書いて下さい",
     max_tokens: 300,
     temperature: 0,
     stream: true
@@ -29,7 +30,6 @@ wrap( async () => {
     timeout: 5000    ,
     responseType: "stream"
   });
-  //console.log("RES:",res);
   res.data.on("data",(data)=> {
     const text=data.toString();
     if(text.includes("[DONE]")) {
@@ -37,7 +37,6 @@ wrap( async () => {
       process.exit(0);
     }
     const json=text.replace(/^data: /,"");
-    //    console.log("json:",json);
     try {
       const obj=JSON.parse(json);
       process.stdout.write(obj.choices[0].text);      
