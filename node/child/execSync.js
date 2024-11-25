d50497641990febb65f47c7769b5ce286da350b6
kengo nakajima <kengo.nakajima@gmail.com>
2024-11-25 14:30:12 +0900

    execSync

---
+++ node/child/execSync.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process');
+
+
+try {
+  const result = execSync('echo hoge >&2; echo fuga >&1');
+  console.log('Success:', result);
+} catch (error) {
+  console.error('Error occurred:',error);
+  if (error.stderr) {
+    console.error('stderr:', error.stderr.toString());
+  }
+  if (error.stdout) {
+    console.error('stdout:', error.stdout.toString());
+  }
+  console.error('Status code:', error.status);
+  console.error('Signal:', error.signal);
+}
