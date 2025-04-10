3806c5ce199c476a04c9cdae2b856c7018074ada
kengo nakajima <kengo.nakajima@gmail.com>
2025-04-10 15:04:29 +0900

    mac_automate

---
+++ mac/cliclick.sh
@@ -0,0 +1,15 @@
+cliclick p
+sleep 1
+cliclick m:396,16
+sleep 1
+cliclick c:.
+sleep 1
+# cliclick t:helloほげ
+echo ほげjapanese | pbcopy
+sleep 1
+#cliclick kp:cmd-v ない
+echo Paste!
+cliclick kd:cmd t:v ku:cmd
+
+
+
